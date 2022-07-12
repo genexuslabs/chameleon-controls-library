2ae01b2459c76c0f103c2ade76b39d0c285b4a40
Daniel Mariño <49991370+dangmarm@users.noreply.github.com>
2022-07-12 15:20:08 -0300

    QR (#16)
    * Add qr component
    
    * Add qr dependecy and component
    
    - Add qr-creator dependency
    - Add ch-qr component
    
    * Merge
    
    Co-authored-by: Bruno Sastre <bsastre@genexus.com>

--- src/components.d.ts
+++ src/components.d.ts
@@ -13,6 +13,7 @@ import { ChGridManager } from "./components/grid/ch-grid-manager";
 import { Color, Size } from "./components/icon/icon";
 import { ChPaginatorActivePageChangedEvent } from "./components/paginator/ch-paginator";
 import { ChPaginatorNavigationClickedEvent, ChPaginatorNavigationType } from "./components/paginator-navigate/ch-paginator-navigate-types";
+import { ecLevel } from "./components/qr/ch-qr";
 import { GxGrid } from "./components/gx-grid/gx-grid-chameleon";
 export namespace Components {
     interface ChFormCheckbox {
@@ -130,6 +131,14 @@ export namespace Components {
         "renderFirstLastPages": true;
         "textDots": string;
         "totalPages": number;
+    }
+    interface ChQr {
+        "background": string | null;
+        "ecLevel": ecLevel;
+        "fill": string;
+        "radius": number;
+        "size": number;
+        "text": string | undefined;
     }
     interface ChSelect {
         "arrowIconSrc": string;
@@ -386,6 +395,12 @@ declare global {
         prototype: HTMLChPaginatorPagesElement;
         new (): HTMLChPaginatorPagesElement;
     };
+    interface HTMLChQrElement extends Components.ChQr, HTMLStencilElement {
+    }
+    var HTMLChQrElement: {
+        prototype: HTMLChQrElement;
+        new (): HTMLChQrElement;
+    };
     interface HTMLChSelectElement extends Components.ChSelect, HTMLStencilElement {
     }
     var HTMLChSelectElement: {
@@ -466,6 +481,7 @@ declare global {
         "ch-paginator": HTMLChPaginatorElement;
         "ch-paginator-navigate": HTMLChPaginatorNavigateElement;
         "ch-paginator-pages": HTMLChPaginatorPagesElement;
+        "ch-qr": HTMLChQrElement;
         "ch-select": HTMLChSelectElement;
         "ch-sidebar-menu": HTMLChSidebarMenuElement;
         "ch-sidebar-menu-list": HTMLChSidebarMenuListElement;
@@ -609,6 +625,14 @@ declare namespace LocalJSX {
         "renderFirstLastPages"?: true;
         "textDots"?: string;
         "totalPages"?: number;
+    }
+    interface ChQr {
+        "background"?: string | null;
+        "ecLevel"?: ecLevel;
+        "fill"?: string;
+        "radius"?: number;
+        "size"?: number;
+        "text"?: string | undefined;
     }
     interface ChSelect {
         "arrowIconSrc"?: string;
@@ -790,6 +814,7 @@ declare namespace LocalJSX {
         "ch-paginator": ChPaginator;
         "ch-paginator-navigate": ChPaginatorNavigate;
         "ch-paginator-pages": ChPaginatorPages;
+        "ch-qr": ChQr;
         "ch-select": ChSelect;
         "ch-sidebar-menu": ChSidebarMenu;
         "ch-sidebar-menu-list": ChSidebarMenuList;
@@ -825,6 +850,7 @@ declare module "@stencil/core" {
             "ch-paginator": LocalJSX.ChPaginator & JSXBase.HTMLAttributes<HTMLChPaginatorElement>;
             "ch-paginator-navigate": LocalJSX.ChPaginatorNavigate & JSXBase.HTMLAttributes<HTMLChPaginatorNavigateElement>;
             "ch-paginator-pages": LocalJSX.ChPaginatorPages & JSXBase.HTMLAttributes<HTMLChPaginatorPagesElement>;
+            "ch-qr": LocalJSX.ChQr & JSXBase.HTMLAttributes<HTMLChQrElement>;
             "ch-select": LocalJSX.ChSelect & JSXBase.HTMLAttributes<HTMLChSelectElement>;
             "ch-sidebar-menu": LocalJSX.ChSidebarMenu & JSXBase.HTMLAttributes<HTMLChSidebarMenuElement>;
             "ch-sidebar-menu-list": LocalJSX.ChSidebarMenuList & JSXBase.HTMLAttributes<HTMLChSidebarMenuListElement>;

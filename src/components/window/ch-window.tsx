db84b5f05fa2a1feabd2273029776260c65d8652
Daniel Mariño <49991370+dangmarm@users.noreply.github.com>
2023-04-19 12:32:35 -0300

    Improve ch-window

--- src/components/window/ch-window.tsx
+++ src/components/window/ch-window.tsx
@@ -1,4 +1,21 @@
-import { Component, h, Host, Prop, Event, EventEmitter } from "@stencil/core";
+import {
+  Component,
+  h,
+  Host,
+  Prop,
+  Event,
+  EventEmitter,
+  Watch,
+  Listen,
+  Element
+} from "@stencil/core";
+
+export type ChWindowAlign =
+  | "outside-start"
+  | "inside-start"
+  | "center"
+  | "inside-end"
+  | "outside-end";
 
 @Component({
   tag: "ch-window",
@@ -6,55 +23,217 @@ import { Component, h, Host, Prop, Event, EventEmitter } from "@stencil/core";
   shadow: true
 })
 export class ChWindow {
+  @Element() el: HTMLChWindowElement;
+  @Prop() readonly container?: HTMLElement;
+  @Prop({ reflect: true, mutable: true }) xAlign: ChWindowAlign = "center";
+  @Prop({ reflect: true, mutable: true }) yAlign: ChWindowAlign = "center";
   @Prop({ reflect: true, mutable: true }) hidden = true;
   @Prop({ reflect: true }) readonly modal: boolean = true;
   @Prop() readonly caption: string = "";
   @Prop() readonly closeText: string;
   @Prop() readonly closeTooltip: string;
-  @Prop() readonly closeAuto: boolean;
+  @Prop() readonly closeOnOutsideClick: boolean;
+  @Prop() readonly closeOnEscape: boolean;
+  @Prop() readonly allowDrag: "no" | "header" | "box" = "no";
 
+  @Event() windowOpened: EventEmitter;
   @Event() windowClosed: EventEmitter;
 
-  private handleMaskClick = (eventInfo: Event) => {
+  private isContainerCssOverride = false;
+  private containerResizeObserver: ResizeObserver;
+  private mask: HTMLElement;
+  private header: HTMLElement;
+  private window: HTMLElement;
+  private readonly validCssAligns: ChWindowAlign[] = [
+    "outside-start",
+    "inside-start",
+    "center",
+    "inside-end",
+    "outside-end"
+  ];
+  private dragStartX: number;
+  private dragStartY: number;
+  private draggedX = 0;
+  private draggedY = 0;
+
+  componentWillLoad() {
+    this.containerResizeObserver = new ResizeObserver(this.updatePosition);
+
+    this.containerResizeObserverHandler(this.container);
+    this.watchCSSAlign();
+  }
+
+  @Watch("container")
+  containerHandler(value: HTMLElement, oldValue: HTMLElement) {
+    this.containerResizeObserverHandler(value, oldValue);
+    this.updatePosition();
+  }
+
+  @Watch("hidden")
+  hiddenHandler() {
+    if (this.hidden) {
+      this.resetDrag();
+      this.windowClosed.emit();
+    } else {
+      this.updatePosition();
+      this.windowOpened.emit();
+    }
+  }
+
+  @Listen("resize", { target: "window", passive: true })
+  windowResizeHandler() {
+    this.watchCSSAlign();
+  }
+
+  @Listen("click", { target: "document", capture: true })
+  documentClickHandler(eventInfo: PointerEvent) {
+    if (
+      this.closeOnOutsideClick &&
+      !eventInfo.composedPath().includes(this.window)
+    ) {
+      this.hidden = true;
+    }
+  }
+
+  @Listen("keydown ", { target: "document", capture: true })
+  documentKeyDownHandler(eventInfo: KeyboardEvent) {
+    if (!this.hidden && this.closeOnEscape && eventInfo.key === "Escape") {
+      this.hidden = true;
+    }
+  }
+
+  @Listen("scroll", { target: "document", capture: true, passive: true })
+  windowScrollHandler() {
+    if (this.container) {
+      this.updatePosition();
+    }
+  }
+
+  @Listen("mousedown", { passive: true })
+  mousedownHandler(eventInfo: MouseEvent) {
+    if (this.isDraggable(eventInfo.composedPath())) {
+      this.dragStartX = eventInfo.clientX;
+      this.dragStartY = eventInfo.clientY;
+
+      document.addEventListener("mousemove", this.mousemoveHandler, {
+        passive: true
+      });
+      document.addEventListener("mouseup", this.mouseupHandler, {
+        once: true
+      });
+    }
+  }
+
+  @Listen("windowCloseClicked")
+  windowCloseClickedHandler() {
+    this.hidden = true;
+  }
+
+  private mousemoveHandler = (eventInfo: MouseEvent) => {
+    const translateX = this.draggedX + eventInfo.clientX - this.dragStartX;
+    const translateY = this.draggedY + eventInfo.clientY - this.dragStartY;
+
+    this.window.style.setProperty("--ch-window-x-drag", `${translateX}px`);
+    this.window.style.setProperty("--ch-window-y-drag", `${translateY}px`);
+  };
+
+  private mouseupHandler = () => {
+    document.removeEventListener("mousemove", this.mousemoveHandler);
+    this.draggedX = parseInt(
+      this.window.style.getPropertyValue("--ch-window-x-drag")
+    );
+    this.draggedY = parseInt(
+      this.window.style.getPropertyValue("--ch-window-y-drag")
+    );
+  };
+
+  private maskClickHandler = (eventInfo: PointerEvent) => {
     eventInfo.stopPropagation();
-    if (this.closeAuto) {
-      this.close();
+  };
+
+  private updatePosition = () => {
+    if (!this.isContainerCssOverride && this.container && this.mask) {
+      const rect = this.container.getBoundingClientRect();
+
+      // TODO: RTL positioning bug
+      this.mask.style.setProperty("inset-inline-start", `${rect.left}px`);
+      this.mask.style.setProperty("inset-block-start", `${rect.top}px`);
+      this.mask.style.width = `${rect.width}px`;
+      this.mask.style.height = `${rect.height}px`;
+    } else if (this.isContainerCssOverride || !this.container) {
+      this.mask.style.removeProperty("inset-inline-start");
+      this.mask.style.removeProperty("inset-block-start");
+      this.mask.style.removeProperty("width");
+      this.mask.style.removeProperty("height");
     }
   };
 
-  private handleCloseClick = (eventInfo: Event) => {
-    eventInfo.stopPropagation();
-    this.close();
-  };
+  private resetDrag() {
+    this.dragStartX = undefined;
+    this.dragStartY = undefined;
+    this.draggedX = 0;
+    this.draggedY = 0;
+    this.window.style.removeProperty("--ch-window-x-drag");
+    this.window.style.removeProperty("--ch-window-y-drag");
+  }
 
-  private handleDialogClick = (eventInfo: Event) => {
-    eventInfo.stopPropagation();
-  };
+  private isDraggable(target: EventTarget[]): boolean {
+    return (
+      this.allowDrag !== "no" &&
+      ((this.allowDrag === "header" && target.includes(this.header)) ||
+        (this.allowDrag === "box" && target.includes(this.window)))
+    );
+  }
 
-  private close() {
-    this.hidden = true;
-    this.windowClosed.emit();
+  private watchCSSAlign() {
+    const style = getComputedStyle(this.el);
+    const container = style.getPropertyValue("--ch-window-container").trim();
+    const xAlign = style
+      .getPropertyValue("--ch-window-x-align")
+      .trim() as ChWindowAlign;
+    const yAlign = style
+      .getPropertyValue("--ch-window-y-align")
+      .trim() as ChWindowAlign;
+
+    this.isContainerCssOverride = container.includes("window") ? true : false;
+
+    if (this.validCssAligns.includes(xAlign)) {
+      this.xAlign = xAlign;
+    }
+    if (this.validCssAligns.includes(yAlign)) {
+      this.yAlign = yAlign;
+    }
+  }
+
+  private containerResizeObserverHandler(
+    observe: HTMLElement,
+    unobserve?: HTMLElement
+  ) {
+    if (observe) {
+      this.containerResizeObserver.observe(observe);
+    }
+    if (unobserve) {
+      this.containerResizeObserver.unobserve(unobserve);
+    }
   }
 
   render() {
     return (
       <Host>
-        <div class="mask" part="mask" onClick={this.handleMaskClick}>
-          <section
-            class="window"
-            part="window"
-            onClick={this.handleDialogClick}
-          >
-            <header part="header">
-              <span part="caption">{this.caption}</span>
-              <button
-                part="close"
-                type="button"
-                title={this.closeTooltip}
-                onClick={this.handleCloseClick}
-              >
-                {this.closeText}
-              </button>
+        <div
+          class="mask"
+          part="mask"
+          ref={el => (this.mask = el)}
+          onClick={this.maskClickHandler}
+        >
+          <section class="window" part="window" ref={el => (this.window = el)}>
+            <header part="header" ref={el => (this.header = el)}>
+              <slot name="header">
+                <span part="caption">{this.caption}</span>
+                <ch-window-close part="close" title={this.closeTooltip}>
+                  {this.closeText}
+                </ch-window-close>
+              </slot>
             </header>
             <main part="main">
               <slot></slot>

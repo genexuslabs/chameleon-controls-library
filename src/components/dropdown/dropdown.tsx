9367291c5deb13ed94caa5ddabbcd2dd9c9163bf
Nicolás Cámera <ncamera@genexus.com>
2023-05-25 19:06:53 -0300

    Add ch-dropdown control
    A generic dropdown menu that has 4 slots. One for the layout of the button that displays the dropdown, one for the dropdown header, one for the dropdown items and the last one for the dropdown footer.

---
+++ src/components/dropdown/dropdown.tsx
@@ -0,0 +1,414 @@
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Host,
+  Listen,
+  Prop,
+  State,
+  Watch,
+  h
+} from "@stencil/core";
+import { Component as ChComponent } from "../../common/interfaces";
+
+const EXPANDABLE_BUTTON_ID = "expandable-button";
+const SECTION_ID = "section";
+
+const DROPDOWN_ITEM_TAG_NAME = "ch-dropdown-item";
+const DROPDOWN_ITEM_SELECTOR = `:scope > ${DROPDOWN_ITEM_TAG_NAME}`;
+
+// Keys
+const TAB_KEY = "Tab";
+type DropDownKeyDownEvents = "ArrowDown" | "ArrowUp" | "Escape";
+
+@Component({
+  shadow: true,
+  styleUrl: "dropdown.scss",
+  tag: "ch-dropdown"
+})
+export class ChDropDown implements ChComponent {
+  private keyEventsDictionary: {
+    [key in DropDownKeyDownEvents]: (event?: KeyboardEvent) => void;
+  } = {
+    ArrowDown: (event: KeyboardEvent) => {
+      event.preventDefault();
+
+      if (!this.currentFocusedItem) {
+        this.focusFirstDropDownItem();
+
+        return;
+      }
+
+      const nextDropDownItem = this.findNextDropDownItemSibling();
+      if (nextDropDownItem) {
+        // This is wrong, it should call the focusElement Method. StencilJS bug?
+        nextDropDownItem.handleFocusElement();
+      }
+    },
+
+    ArrowUp: (event: KeyboardEvent) => {
+      event.preventDefault();
+
+      if (!this.currentFocusedItem) {
+        this.focusFirstDropDownItem();
+
+        return;
+      }
+
+      const previousDropDownItem = this.findPreviousDropDownItemSibling();
+      if (previousDropDownItem) {
+        // This is wrong, it should call the focusElement Method. StencilJS bug?
+        previousDropDownItem.handleFocusElement();
+      }
+    },
+
+    Escape: () => {
+      this.closeDropdown();
+      this.returnFocusToButton();
+    }
+  };
+
+  private showHeader = false;
+  private showFooter = false;
+
+  /**
+   * Determine the current dropdown-item that is focused
+   */
+  private currentFocusedItem: HTMLChDropdownItemElement;
+
+  // Refs
+  private expandableButton: HTMLButtonElement;
+
+  @Element() element: HTMLChDropdownElement;
+
+  @State() expanded = false;
+  @State() expandedWithHover = false;
+
+  /**
+   * Specifies the horizontal alignment the dropdown section has when using
+   * `position === "Top"` or `position === "Bottom"`.
+   */
+  @Prop() readonly align: "Left" | "Center" | "Right" = "Center";
+
+  /**
+   * This attribute lets you specify the label for the expandable button.
+   * Important for accessibility.
+   */
+  @Prop() readonly buttonLabel: string = "Show options";
+
+  /**
+   * Determine which actions on the expandable button display the dropdown
+   * section.
+   */
+  @Prop() readonly expandBehavior: "Click" | "Click or Hover" =
+    "Click or Hover";
+
+  /**
+   * Determine if the dropdown section should be opened when the expandable
+   * button of the control is focused.
+   */
+  @Prop() readonly openOnFocus: boolean = false;
+
+  /**
+   * Specifies the position of the dropdown section that is placed relative to
+   * the expandable button.
+   */
+  @Prop() readonly position: "Top" | "Right" | "Bottom" | "Left" = "Bottom";
+
+  /**
+   * Specifies the separation (in pixels) between the expandable button and the
+   * dropdown section of the control.
+   */
+  @Prop() readonly dropdownSeparation: number = 12;
+
+  /**
+   * Specifies the vertical alignment the dropdown section has when using
+   * `position === "Right"` or `position === "Left"`.
+   */
+  @Prop() readonly valign: "Top" | "Middle" | "Bottom" = "Middle";
+
+  /**
+   * Fired when the visibility of the dropdown section is changed
+   */
+  @Event() expandedChange: EventEmitter<boolean>;
+
+  @Watch("expanded")
+  handleExpandedChange(newExpandedValue: boolean) {
+    if (newExpandedValue) {
+      this.currentFocusedItem = undefined;
+
+      // Click
+      document.body.addEventListener(
+        "click",
+        this.closeDropdownWhenClickingOutside,
+        {
+          capture: true
+        }
+      );
+
+      // Keyboard events
+      document.body.addEventListener("keydown", this.handleKeyDownEvents, {
+        capture: true
+      });
+      document.body.addEventListener("keyup", this.handleKeyUpEvents, {
+        capture: true
+      });
+    } else {
+      // Click
+      document.body.removeEventListener(
+        "click",
+        this.closeDropdownWhenClickingOutside,
+        {
+          capture: true
+        }
+      );
+
+      // Keyboard events
+      document.body.removeEventListener("keydown", this.handleKeyDownEvents, {
+        capture: true
+      });
+      document.body.removeEventListener("keyup", this.handleKeyUpEvents, {
+        capture: true
+      });
+    }
+  }
+
+  @Listen("actionClick")
+  handleActionClick() {
+    this.closeDropdown();
+
+    // @todo This behavior must be specified by a property
+    // this.returnFocusToButton();
+  }
+
+  @Listen("focusChange")
+  handleDropDownItemFocusChange(event: CustomEvent) {
+    this.currentFocusedItem = event.target as HTMLChDropdownItemElement;
+  }
+
+  private focusFirstDropDownItem() {
+    this.currentFocusedItem = this.element.querySelector(
+      DROPDOWN_ITEM_SELECTOR
+    );
+
+    if (this.currentFocusedItem) {
+      this.currentFocusedItem.handleFocusElement();
+    }
+  }
+
+  private findNextDropDownItemSibling() {
+    let nextSibling = this.currentFocusedItem
+      .nextElementSibling as HTMLChDropdownItemElement;
+
+    while (
+      nextSibling &&
+      nextSibling.tagName.toLowerCase() !== DROPDOWN_ITEM_TAG_NAME
+    ) {
+      nextSibling = nextSibling.nextElementSibling as HTMLChDropdownItemElement;
+    }
+
+    return nextSibling;
+  }
+
+  private findPreviousDropDownItemSibling() {
+    let previousSibling = this.currentFocusedItem
+      .previousElementSibling as HTMLChDropdownItemElement;
+
+    while (
+      previousSibling &&
+      previousSibling.tagName.toLowerCase() !== DROPDOWN_ITEM_TAG_NAME
+    ) {
+      previousSibling =
+        previousSibling.previousElementSibling as HTMLChDropdownItemElement;
+    }
+
+    return previousSibling;
+  }
+
+  private closeDropdown = () => {
+    this.closeDropdownWithHover();
+    this.expanded = false;
+    this.expandedChange.emit(false);
+  };
+
+  private closeDropdownWithHover = () => {
+    this.expandedWithHover = false;
+  };
+
+  /**
+   * Returns focus to the expandable button when closing the dropdown. Only
+   * works if `openOnFocus = "false"`
+   */
+  private returnFocusToButton() {
+    if (!this.openOnFocus) {
+      this.expandableButton.focus();
+    }
+  }
+
+  private closeDropdownWhenClickingOutside = (event: MouseEvent) => {
+    if (event.composedPath().find(el => el === this.element) === undefined) {
+      this.closeDropdown();
+    }
+  };
+
+  private handleKeyDownEvents = (event: KeyboardEvent) => {
+    const keyEventHandler: ((event?: KeyboardEvent) => void) | undefined =
+      this.keyEventsDictionary[event.code];
+
+    if (keyEventHandler) {
+      keyEventHandler(event);
+    }
+  };
+
+  /**
+   * Check if the next focused element is a child element of the dropdown
+   * control.
+   */
+  private handleKeyUpEvents = (event: KeyboardEvent) => {
+    if (event.code !== TAB_KEY) {
+      return;
+    }
+    const nextFocusedElement = event.target as HTMLElement;
+
+    const isChildElement =
+      nextFocusedElement.closest("ch-dropdown") === this.element;
+    if (isChildElement) {
+      return;
+    }
+
+    this.closeDropdown();
+  };
+
+  private handleMouseLeave = () => {
+    const focusedElementIsInsideDropDown =
+      document.activeElement.closest("ch-dropdown") === this.element;
+
+    if (focusedElementIsInsideDropDown) {
+      this.expanded = true;
+    }
+    this.closeDropdownWithHover();
+  };
+
+  private handleMouseEnter = () => {
+    if (this.expandedWithHover) {
+      return;
+    }
+
+    this.expandedWithHover = true;
+
+    // If not previously expanded, emit the event
+    if (!this.expanded) {
+      this.expandedChange.emit(true);
+    }
+  };
+
+  private handleButtonClick = () => {
+    this.expandedChange.emit(!this.expanded);
+    this.expanded = !this.expanded;
+  };
+
+  private handleButtonFocus = () => {
+    if (this.expanded) {
+      return;
+    }
+
+    this.expanded = true;
+
+    // If not previously expanded, emit the event
+    if (!this.expandedWithHover) {
+      this.expandedChange.emit(true);
+    }
+  };
+
+  componentWillLoad() {
+    this.showHeader = !!this.element.querySelector(':scope > [slot="header"]');
+    this.showFooter = !!this.element.querySelector(':scope > [slot="footer"]');
+  }
+
+  render() {
+    const hasVerticalPosition =
+      this.position === "Bottom" || this.position === "Top";
+
+    const isExpanded = this.expanded || this.expandedWithHover;
+
+    return (
+      <Host
+        onMouseLeave={
+          this.expandBehavior === "Click or Hover"
+            ? this.handleMouseLeave
+            : undefined
+        }
+        style={{
+          "--separation-between-button": `-${this.dropdownSeparation}px`,
+          "--separation-between-button-size": `${this.dropdownSeparation}px`
+        }}
+      >
+        <button
+          id={EXPANDABLE_BUTTON_ID}
+          aria-controls={SECTION_ID}
+          aria-expanded={this.expanded.toString()}
+          aria-haspopup="true"
+          aria-label={this.buttonLabel}
+          class="expandable-button"
+          part="dropdown__expandable-button"
+          type="button"
+          onClick={this.handleButtonClick}
+          onFocus={this.openOnFocus ? this.handleButtonFocus : undefined}
+          onMouseEnter={
+            this.expandBehavior === "Click or Hover"
+              ? this.handleMouseEnter
+              : undefined
+          }
+          ref={el => (this.expandableButton = el)}
+        >
+          <slot name="action" />
+        </button>
+
+        {this.expandBehavior === "Click or Hover" && (
+          // Necessary since the separation between the button and the section
+          // triggers the onMouseLeave event
+          <div
+            aria-hidden="true"
+            class={{
+              "dummy-separation": true,
+              [`dummy-separation--${this.position.toLowerCase()}`]: true,
+              "dummy-separation--vertical": hasVerticalPosition,
+              "dummy-separation--horizontal": !hasVerticalPosition
+            }}
+          ></div>
+        )}
+
+        <section
+          id={SECTION_ID}
+          aria-labelledby={EXPANDABLE_BUTTON_ID}
+          class={{
+            section: true,
+            [`position--${this.position.toLowerCase()}`]: true,
+
+            [`align--${this.align.toLowerCase()}`]: hasVerticalPosition,
+            [`valign--${this.valign.toLowerCase()}`]: !hasVerticalPosition
+          }}
+          part="dropdown__section"
+          hidden={!isExpanded}
+        >
+          {this.showHeader && (
+            <header class="header" part="dropdown__header">
+              <slot name="header" />
+            </header>
+          )}
+
+          <div role="list" class="list" part="dropdown__list">
+            <slot name="items" />
+          </div>
+
+          {this.showFooter && (
+            <footer class="footer" part="dropdown__footer">
+              <slot name="footer" />
+            </footer>
+          )}
+        </section>
+      </Host>
+    );
+  }
+}
---
+++ src/components/dropdown/readme.md
@@ -0,0 +1,81 @@
+# ch-dropdown
+
+A generic dropdown menu that has 4 slots. One for the layout of the button that
+displays the dropdown, one for the dropdown header, one for the dropdown items
+and the last one for the dropdown footer.
+
+```html
+<ch-dropdown
+  align="Center"
+  class="Class"
+  dropdown-separation="5"
+  expand-behavior="Click or hover"
+  open-on-focus="false"
+  position="Bottom"
+>
+  <span slot="action">User info</span>
+
+  <div slot="header">
+    <h1>John Doe</h1>
+    <span>johndoe@example.com</span>
+  </div>
+
+  <ch-dropdown-item
+    slot="items"
+    class="dropdown-item--fancy"
+    href="https://..."
+    leftImgSrc="./MyProfile.svg"
+    rightImgSrc="AnotherImg"
+  >
+    My profile
+  </ch-dropdown-item>
+
+  <ch-dropdown-item-separator slot="items"></ch-dropdown-item-separator>
+
+  <ch-dropdown-item
+    slot="items"
+    id="2"
+    leftImgSrc="./Logout.svg"
+    rightImgSrc="item.RightImage"
+  >
+    Logout
+  </ch-dropdown-item>
+
+  <div slot="footer">...</div>
+</ch-dropdown>
+```
+
+<!-- Auto Generated Below -->
+
+## Properties
+
+| Property             | Attribute             | Description                                                                                                             | Type                                     | Default            |
+| -------------------- | --------------------- | ----------------------------------------------------------------------------------------------------------------------- | ---------------------------------------- | ------------------ |
+| `align`              | `align`               | Specifies the horizontal alignment the dropdown section has when using `position === "Top"` or `position === "Bottom"`. | `"Center" \| "Left" \| "Right"`          | `"Center"`         |
+| `buttonLabel`        | `button-label`        | This attribute lets you specify the label for the expandable button. Important for accessibility.                       | `string`                                 | `"Show options"`   |
+| `dropdownSeparation` | `dropdown-separation` | Specifies the separation (in pixels) between the expandable button and the dropdown section of the control.             | `number`                                 | `12`               |
+| `expandBehavior`     | `expand-behavior`     | Determine which actions on the expandable button display the dropdown section.                                          | `"Click or Hover" \| "Click"`            | `"Click or Hover"` |
+| `openOnFocus`        | `open-on-focus`       | Determine if the dropdown section should be opened when the expandable button of the control is focused.                | `boolean`                                | `false`            |
+| `position`           | `position`            | Specifies the position of the dropdown section that is placed relative to the expandable button.                        | `"Bottom" \| "Left" \| "Right" \| "Top"` | `"Bottom"`         |
+| `valign`             | `valign`              | Specifies the vertical alignment the dropdown section has when using `position === "Right"` or `position === "Left"`.   | `"Bottom" \| "Middle" \| "Top"`          | `"Middle"`         |
+
+## Events
+
+| Event            | Description                                                  | Type                   |
+| ---------------- | ------------------------------------------------------------ | ---------------------- |
+| `expandedChange` | Fired when the visibility of the dropdown section is changed | `CustomEvent<boolean>` |
+
+## Shadow Parts
+
+| Part                            | Description |
+| ------------------------------- | ----------- |
+| `"dropdown__expandable-button"` |             |
+| `"dropdown__footer"`            |             |
+| `"dropdown__header"`            |             |
+| `"dropdown__list"`              |             |
+| `"dropdown__section"`           |             |
+
+---
+
+_Built with [StencilJS](https://stenciljs.com/)_
+

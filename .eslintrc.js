1ef29f565d4eb0503446226d68107389a5245081
Nicolás Cámera <ncamera@genexus.com>
2023-07-23 10:55:36 -0300

    Bump stencil/eslint-plugin from 0.4.0 to 0.5.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin,
-    "eslint:recommended", // Uses the recommended rules from the eslint,
-    "plugin:@stencil/recommended",
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@stencil-community/recommended", // Enables @stencil-community/eslint-plugin.
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
@@ -25,12 +24,15 @@ module.exports = {
       }
     ],
 
-    "@stencil/async-methods": "warn", // This rule catches Stencil public methods that are not async
-    "@stencil/decorators-context": "warn", // This rule catches Stencil decorators in bad locations
-    "@stencil/decorators-style": [
+    // - - - - - - - - - - - -
+    // StencilJS
+    // - - - - - - - - - - - -
+    "@stencil-community/async-methods": "warn", // This rule catches Stencil public methods that are not async
+    "@stencil-community/decorators-context": "warn", // This rule catches Stencil decorators in bad locations
+    "@stencil-community/decorators-style": [
       "warn",
       {
-        prop: "inline",
+        // prop: "inline", // And multiline
         state: "inline",
         element: "inline",
         method: "multiline",
@@ -38,18 +40,20 @@ module.exports = {
         listen: "multiline"
       }
     ],
-    "@stencil/element-type": "warn", // This rule catches Stencil public methods that are not async
-    "@stencil/methods-must-be-public": "warn", // This rule catches Stencil Methods marked as private or protected
-    "@stencil/no-unused-watch": "warn", // This rule catches Stencil Watchs with non existing Props or States
-    "@stencil/own-methods-must-be-private": "warn", // This rule catches own class methods marked as public
-    "@stencil/own-props-must-be-private": "warn", // This rule catches own class properties marked as public
-    "@stencil/prefer-vdom-listener": "warn", // This rule catches Stencil Listen with vdom events
-    "@stencil/props-must-be-public": "warn", // This rule catches Stencil Props marked as private or protected
-    "@stencil/props-must-be-readonly": "warn", // This rule catches Stencil Props marked as non readonly, excluding mutable ones
-    "@stencil/required-jsdoc": "warn", // This rule catches Stencil Props, Methods and Events to define jsdoc
-    "@stencil/required-prefix": ["warn", ["ch-"]], // Ensures that a Component's tag use the "ch-" prefix.
-    "@stencil/single-export": "warn", // This rule catches modules that expose more than just the Stencil Component itself
-    "@stencil/strict-boolean-conditions": "off",
-    "@stencil/strict-mutable": "off" // This rule catches Stencil Prop marked as mutable but not changing value in code
+    "@stencil-community/element-type": "warn", // This rule catches Stencil Element decorator have the correct type
+    "@stencil-community/methods-must-be-public": "warn", // This rule catches Stencil Methods marked as private or protected
+    "@stencil-community/no-unused-watch": "warn", // This rule catches Stencil Watchs with non existing Props or States
+    "@stencil-community/own-methods-must-be-private": "warn", // This rule catches own class methods marked as public
+    "@stencil-community/own-props-must-be-private": "warn", // This rule catches own class properties marked as public
+    "@stencil-community/prefer-vdom-listener": "warn", // This rule catches Stencil Listen with vdom events
+    "@stencil-community/props-must-be-public": "warn", // This rule catches Stencil Props marked as private or protected
+    "@stencil-community/props-must-be-readonly": "warn", // This rule catches Stencil Props marked as non readonly, excluding mutable ones
+    "@stencil-community/required-jsdoc": "warn", // This rule catches Stencil Props, Methods and Events to define jsdoc
+    "@stencil-community/required-prefix": ["warn", ["ch-"]], // Ensures that a Component's tag use the "ch-" prefix.
+    "@stencil-community/single-export": "warn", // This rule catches modules that expose more than just the Stencil Component itself
+    "@stencil-community/strict-boolean-conditions": "off",
+
+    // WA to fix false positive errors
+    "@stencil-community/strict-mutable": "warn" // This rule catches Stencil Prop marked as mutable but not changing value in code
   }
 };

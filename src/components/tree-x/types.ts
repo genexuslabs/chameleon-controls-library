10c1be33c6cfb5b531a6937c1ccc5357872eb29a
Nicolás Cámera <ncamera@genexus.com>
2023-08-30 18:52:39 -0300

    Add types for tree-x model

---
+++ src/components/tree-x/types.ts
@@ -0,0 +1,40 @@
+export type TreeXModel = {
+  items: TreeXItemModel[];
+  multiSelection?: boolean;
+  showLines?: boolean;
+};
+
+export type TreeXItemModel = {
+  id: string;
+  caption: string;
+  checkbox?: boolean;
+  checked?: boolean;
+  cssClass?: string;
+  disabled?: boolean;
+  expanded?: boolean;
+  lazy?: boolean;
+  leaf?: boolean;
+  leftImgSrc?: string;
+  items?: TreeXItemModel[];
+  rightImgSrc?: string;
+  selected?: boolean;
+  showExpandableButton?: boolean;
+  toggleCheckboxes?: boolean;
+};
+
+export type CheckedTreeItemInfo = {
+  id: string;
+  caption: string;
+  selected: boolean;
+};
+
+export type ExpandedTreeItemInfo = {
+  id: string;
+  expanded: boolean;
+};
+
+export type SelectedTreeItemInfo = {
+  id: string;
+  caption: string;
+  checked: boolean;
+};

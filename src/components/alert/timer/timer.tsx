eaaf0509e47a8236adbe155410d233163d9d04f7
vwillenpart <18217253+vwillenpart@users.noreply.github.com>
2023-09-14 11:36:10 -0300

    timer updates

--- src/components/alert/timer/timer.tsx
+++ src/components/alert/timer/timer.tsx
@@ -2,9 +2,10 @@ import { Component, Host, h, Prop } from "@stencil/core";
 
 @Component({
   tag: "ch-timer",
+  styleUrl: "timer.scss",
   shadow: true
 })
-export class ChTiner {
+export class ChTimer {
   /**
    * Sets the progress propiety to determine the progress.
    */
@@ -15,16 +16,39 @@ export class ChTiner {
    */
   @Prop() readonly accessibleName: string = "timer";
 
+  /**
+   * Sets the animationTime to set the custom var for the css animation.
+   */
+  @Prop() readonly animationTime: number = 0;
+
+  /**
+   * Sets the presented property to handle the component presentation.
+   */
+  @Prop() readonly presented: boolean = false;
+
   render() {
     const accessibilityAttributes = {
       role: "timer",
-      "aria-atomic": "true",
+      "aria-live": "true",
       "aria-labelledby": this.accessibleName,
       "aria-valuemin": "0",
       "aria-valuemax": "100",
       "aria-valuenow": this.progress
     };
 
-    return <Host {...accessibilityAttributes}>{this.progress}</Host>;
+    return (
+      <Host>
+        {this.presented && this.progress >= 0 && (
+          <div
+            {...accessibilityAttributes}
+            part="indicator"
+            class="indicator"
+            style={{
+              "--animation-time": `${this.animationTime}ms`
+            }}
+          ></div>
+        )}
+      </Host>
+    );
   }
 }

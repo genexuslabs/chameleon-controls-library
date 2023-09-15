d35491e25fb00020fae01b4f98461728f2ccb7b7
Nicolás Cámera <ncamera@genexus.com>
2023-09-15 18:06:50 -0300

    Reduce CLS when adding actions at runtime

--- src/components/action-group-item/action-group-item.tsx
+++ src/components/action-group-item/action-group-item.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from "@stencil/core";
+import { Component, Element, h, Prop } from "@stencil/core";
 import { Component as ChComponent } from "../../common/interfaces";
 
 /**
@@ -10,12 +10,33 @@ import { Component as ChComponent } from "../../common/interfaces";
   shadow: false
 })
 export class ChActionGroupItem implements ChComponent {
+  @Element() el: HTMLChActionGroupItemElement;
+
+  /**
+   * `true` to ignore the floating property value.
+   */
+  @Prop() readonly avoidFloating: boolean = false;
+
   /**
    * `true` if the control is floating. Useful to implement the
    * `"ResponsiveCollapse"` value for the `itemsOverflowBehavior` property of
    * the ch-action-group control.
    */
-  @Prop({ reflect: true }) readonly floating: boolean = false;
+  @Prop({ reflect: true, mutable: true }) floating = false;
+
+  componentWillLoad() {
+    if (this.avoidFloating) {
+      return;
+    }
+
+    const parentAction = this.el.closest("ch-action-group");
+
+    // Hide items at the start to improve CLS
+    if (parentAction) {
+      this.floating =
+        parentAction.itemsOverflowBehavior === "ResponsiveCollapse";
+    }
+  }
 
   render() {
     return <slot />;

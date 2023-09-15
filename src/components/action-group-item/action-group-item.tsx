798545d0ceb2453633b7885ecbfaf6e1015f89d3
Nicolás Cámera <ncamera@genexus.com>
2023-09-15 01:11:00 -0300

    Refactoring for the action-group's implementation

--- src/components/action-group-item/action-group-item.tsx
+++ src/components/action-group-item/action-group-item.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, Prop } from "@stencil/core";
 import { Component as ChComponent } from "../../common/interfaces";
 
 /**
@@ -10,6 +10,13 @@ import { Component as ChComponent } from "../../common/interfaces";
   shadow: false
 })
 export class ChActionGroupItem implements ChComponent {
+  /**
+   * `true` if the control is floating. Useful to implement the
+   * `"ResponsiveCollapse"` value for the `itemsOverflowBehavior` property of
+   * the ch-action-group control.
+   */
+  @Prop({ reflect: true }) readonly floating: boolean = false;
+
   render() {
     return <slot />;
   }
--- src/components/action-group/action-group.tsx
+++ src/components/action-group/action-group.tsx
@@ -6,21 +6,58 @@ import {
   Prop,
   State,
   Event,
-  EventEmitter
+  EventEmitter,
+  Watch
 } from "@stencil/core";
+import { DropdownPosition } from "../dropdown/types";
+import { ItemsOverflowBehavior } from "./types";
 
+const FLOATING_POINT_ERROR = 1;
+
+/**
+ * @part actions - The container of the visible actions.
+ * @part more-actions - The ch-dropdown control to show hidden actions when `itemsOverflowBehavior === "ResponsiveCollapse"`.
+ *
+ * @slot items - The slot for the actions.
+ * @slot more-items - The slot for the hidden actions when `itemsOverflowBehavior === "ResponsiveCollapse"`.
+ */
 @Component({
   tag: "ch-action-group",
   styleUrl: "action-group.scss",
   shadow: true
 })
 export class ChActionGroup {
+  private needForRAF = true; // To prevent redundant RAF (request animation frame) calls
+
+  private totalItems = -1;
+
+  // Observer
+  private actionsContainerWatcher: ResizeObserver;
+  private actionsWatcher: ResizeObserver;
+
+  // Refs
+  private actionsContainer: HTMLDivElement;
+  private slotItems: HTMLSlotElement;
+
   @Element() el: HTMLChActionGroupElement;
 
   /**
-   * The items inside the actions menu button.
+   * The visible actions when `itemsOverflowBehavior === "ResponsiveCollapse"`.
    */
-  @State() actionsButtonItems: HTMLChActionGroupItemElement[] = [];
+  @State() displayedItems = -1;
+
+  /**
+   * Specifies a short string, typically 1 to 3 words, that authors associate
+   * with an element to provide users of assistive technologies with a label
+   * for the element.
+   */
+  @Prop() readonly accessibleName: string;
+
+  /**
+   * This attribute lets you specify the label for the more actions button.
+   * Important for accessibility.
+   */
+  @Prop() readonly buttonLabel: string = "More actions";
 
   /**
    * This attribute determines how items behave when the content of the ActionGroup overflows horizontal. This property is needed
@@ -32,12 +69,32 @@ export class ChActionGroup {
    * | `Multiline`           | The ActionGroup items that overflow horizontally are shown in a second line of the control.      |
    * | `Responsive Collapse` | The Action Group items, when they start to overflow the control, are placed in the More Actions. |
    */
-  @Prop() readonly itemsOverflowBehavior:
-    | "AddScroll"
-    | "Multiline"
-    | "ResponsiveCollapse" = "ResponsiveCollapse";
+  @Prop() readonly itemsOverflowBehavior: ItemsOverflowBehavior =
+    "ResponsiveCollapse";
+  @Watch("itemsOverflowBehavior")
+  handleOverflowBehaviorChange(newValue: ItemsOverflowBehavior) {
+    if (newValue !== "ResponsiveCollapse") {
+      const actionGroupItems =
+        this.slotItems.assignedElements() as HTMLChActionGroupItemElement[];
+
+      // Reset floating
+      actionGroupItems.forEach(item => {
+        item.floating = false;
+      });
+    }
+
+    this.setResponsiveCollapse();
+  }
 
   /**
+   * Determine which mouse actions on the expandable button display the dropdown
+   * section.
+   */
+  @Prop() readonly expandBehavior: "Click" | "Click or Hover" =
+    "Click or Hover";
+
+  /**
+   * @todo Check a better convention for this property, for example, "ActionsInlineAlignment"
    * This attribute determines the position of the More Actions button in the Action Group.
    *
    * | Value   | Details                                                               |
@@ -48,21 +105,171 @@ export class ChActionGroup {
   @Prop() readonly moreActionsButtonPosition: "Start" | "End" = "Start";
 
   /**
-   * The index of item action that is targeted.
+   * Specifies the position of the dropdown section that is placed relative to
+   * the more actions button.
    */
-  @Prop({ mutable: true }) openIndex: number = null;
+  @Prop() readonly moreActionsDropdownPosition: DropdownPosition =
+    "InsideStart_OutsideEnd";
 
   /**
-   *  When it's true and an action is hovered show the actions menu.
+   * Determine if the dropdowns should be opened when the action is focused.
    */
-  @Prop() readonly showActionsMenuOnHover: boolean = true;
+  @Prop() readonly openOnFocus: boolean = false;
 
   /**
    * Fired when the item is targeted or not.
    */
   @Event() displayedItemsCountChange: EventEmitter<number>;
 
+  /**
+   * Update the visibility of the actions.
+   * Only works if itemsOverflowBehavior === "ResponsiveCollapse"
+   */
+  private updateDisplayedActions = () => {
+    const actionGroupItems =
+      this.slotItems.assignedElements() as HTMLChActionGroupItemElement[];
+
+    // The column-gap property must be taken into account
+    const columnGap = getComputedStyle(this.actionsContainer).columnGap;
+    const columnGapValue =
+      columnGap != null && columnGap.endsWith("px")
+        ? Number(columnGap.replace("px", ""))
+        : 0;
+
+    // Since the last item does not add column-gap, we have to adjust the measurement
+    let availableWidth =
+      this.actionsContainer.clientWidth + columnGapValue - FLOATING_POINT_ERROR;
+    let displayedItems = 0;
+
+    // Check which items are visible
+    actionGroupItems.forEach(action => {
+      const actionWidth = action.clientWidth;
+
+      availableWidth -= actionWidth + columnGapValue;
+
+      if (availableWidth >= 0) {
+        action.floating = false;
+        displayedItems++;
+      } else {
+        action.floating = true;
+      }
+    });
+
+    this.totalItems = actionGroupItems.length;
+    this.displayedItems = displayedItems;
+    this.displayedItemsCountChange.emit(displayedItems);
+  };
+
+  private updateDisplayedActionInFrame = () => {
+    if (!this.needForRAF) {
+      return;
+    }
+    this.needForRAF = false; // No need to call RAF up until next frame
+
+    requestAnimationFrame(() => {
+      this.needForRAF = true; // RAF now consumes the movement instruction so a new one can come
+      this.updateDisplayedActions();
+    });
+  };
+
+  private connectActionsObserver() {
+    this.actionsWatcher = new ResizeObserver(this.updateDisplayedActionInFrame);
+
+    // Observe the actions
+    const actionGroupItems = this.slotItems.assignedElements();
+    actionGroupItems.forEach(action => {
+      this.actionsWatcher.observe(action);
+    });
+  }
+
+  private connectActionsContainerObserver() {
+    this.actionsContainerWatcher = new ResizeObserver(
+      this.updateDisplayedActionInFrame
+    );
+
+    this.actionsContainerWatcher.observe(this.el);
+  }
+
+  private disconnectActionsObserver() {
+    if (this.actionsWatcher) {
+      this.actionsWatcher.disconnect();
+      this.actionsWatcher = null;
+    }
+  }
+
+  private disconnectActionsContainerObserver() {
+    if (this.actionsContainerWatcher) {
+      this.actionsContainerWatcher.disconnect();
+      this.actionsContainerWatcher = null;
+    }
+  }
+
+  private setResponsiveCollapse() {
+    this.disconnectActionsObserver();
+    this.disconnectActionsContainerObserver();
+
+    if (this.itemsOverflowBehavior !== "ResponsiveCollapse") {
+      return;
+    }
+    this.connectActionsObserver();
+    this.connectActionsContainerObserver();
+  }
+
+  private updateActionsWatcher = () => {
+    if (this.itemsOverflowBehavior !== "ResponsiveCollapse") {
+      return;
+    }
+
+    // Avoid memory leaks by disconnecting and re-connecting the observer
+    this.disconnectActionsObserver();
+    this.connectActionsObserver();
+  };
+
+  componentDidLoad() {
+    this.setResponsiveCollapse();
+  }
+
+  disconnectedCallback() {
+    this.disconnectActionsObserver();
+    this.disconnectActionsContainerObserver();
+  }
+
   render() {
-    return <Host role="menubar"></Host>;
+    // @todo TODO: Improve accessibility and keyboard navigation
+
+    return (
+      <Host role="menubar" aria-label={this.accessibleName}>
+        {this.itemsOverflowBehavior === "ResponsiveCollapse" &&
+          this.totalItems !== this.displayedItems && (
+            <ch-dropdown
+              exportparts="expandable-button,separation,list,section,mask,header,footer"
+              buttonLabel={this.buttonLabel}
+              class="more-actions"
+              part="more-actions"
+              expandBehavior={this.expandBehavior}
+              openOnFocus={this.openOnFocus}
+              position={this.moreActionsDropdownPosition}
+            >
+              <slot name="more-items" slot="items"></slot>
+            </ch-dropdown>
+          )}
+
+        <div
+          class={{
+            actions: true,
+            "actions--scroll": this.itemsOverflowBehavior === "AddScroll",
+            "actions--multiline": this.itemsOverflowBehavior === "Multiline"
+          }}
+          part="actions"
+          ref={el => (this.actionsContainer = el)}
+        >
+          <slot
+            name="items"
+            onSlotchange={this.updateActionsWatcher}
+            ref={el => (this.slotItems = el as HTMLSlotElement)}
+          ></slot>
+        </div>
+      </Host>
+    );
   }
 }

b83c7ee01bc533d8aa113503d7ed02b339ca676f
Daniel Mariño <49991370+dangmarm@users.noreply.github.com>
2023-10-03 14:13:06 -0300

    Support "append" styles. Add functions to hide global style.

--- src/components/style/ch-global-stylesheet.ts
+++ src/components/style/ch-global-stylesheet.ts
@@ -1,1 +1,17 @@
-export const CH_GLOBAL_STYLESHEET = new CSSStyleSheet({ disabled: true });
+const CH_GLOBAL_STYLESHEET = new CSSStyleSheet({ disabled: true });
+
+export function adoptGlobalStyleSheet(adoptedStyleSheets: CSSStyleSheet[]) {
+  adoptedStyleSheets.push(CH_GLOBAL_STYLESHEET);
+}
+
+export function appendStyle(text: string) {
+  const styleSheetParser = new CSSStyleSheet({ disabled: true });
+
+  styleSheetParser.replace(text);
+  for (let i = 0; i < styleSheetParser.cssRules.length; i++) {
+    CH_GLOBAL_STYLESHEET.insertRule(styleSheetParser.cssRules.item(i).cssText);
+  }
+}
+export function enableStyleSheet() {
+  CH_GLOBAL_STYLESHEET.disabled = false;
+}

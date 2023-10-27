5dbffc657566a4fde74c6416fc41c51217ec3607
Nicolás Cámera <ncamera@genexus.com>
2023-10-27 15:33:42 -0300

    Optimize Tree View bundle

--- stencil.config.ts
+++ stencil.config.ts
@@ -34,7 +34,7 @@ export const config: Config = {
       components: ["ch-next-data-modeling", "ch-next-data-modeling-item"]
     },
     {
-      components: ["ch-tree-view", "ch-tree-view-item"]
+      components: ["ch-tree-view", "ch-tree-view-item", "ch-tree-view-render"]
     }
   ]
 };

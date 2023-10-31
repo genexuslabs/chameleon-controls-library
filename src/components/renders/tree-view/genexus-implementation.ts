da55355f2893dc416945c2e51e5b5556200ca001
Nicolás Cámera <81186472+ncamera@users.noreply.github.com>
2023-10-31 15:12:06 -0300

    Add workarounds to add support for using the Tree View in GeneXus (#172)
    * Adjust the condition for the renderItem function
    
    * Add WAs to support rendering the Tree View with GeneXus' UC 2.0

---
+++ src/components/renders/tree-view/genexus-implementation.ts
@@ -0,0 +1,131 @@
+export type TreeViewGXItemModel = {
+  id: string;
+  caption: string;
+  checkbox?: boolean;
+  checked?: boolean;
+  class?: string;
+  downloading?: boolean;
+  dragEnabled?: boolean;
+  dropEnabled?: boolean;
+  editable?: boolean;
+  enabled?: boolean;
+  expanded?: boolean;
+
+  /**
+   * Used by the tree view to decide which is the last item in the list when
+   * filters are applied.
+   */
+  lastItemId?: string;
+
+  lazy?: boolean;
+  leaf?: boolean;
+  leftImage?: string;
+  indeterminate?: boolean;
+  items?: TreeViewGXItemModel[];
+  metadata?: string;
+
+  /**
+   * Establish the order at which the item will be placed in its parent.
+   * Multiple items can have the same `order` value.
+   */
+  order?: number;
+
+  /**
+   * `false` to not render the item.
+   */
+  render?: boolean;
+  rightImage?: string;
+  selected?: boolean;
+  showExpandableButton?: boolean;
+  toggleCheckboxes?: boolean;
+};
+
+const URL_REGEX = /url\((["']?)([^\)]*)\)(?:\s+([\d.]+)x)?/i;
+let computedStyle: CSSStyleDeclaration;
+
+function normalizeUri(uri: string): string {
+  if (uri.startsWith("data:image/svg+xml;utf8,")) {
+    uri = `data:image/svg+xml;base64,${btoa(
+      uri.slice(24).replace(/\\"/g, '"')
+    )}`;
+  }
+
+  uri = uri.replace(/\\/g, "");
+  uri = uri.replace(/\s/g, "%20");
+
+  return uri;
+}
+
+function getImage(
+  name: string,
+  gxImageConstructor: (name: string) => any
+): any {
+  if (!computedStyle) {
+    computedStyle = getComputedStyle(document.documentElement);
+  }
+
+  let value = computedStyle.getPropertyValue(`--gx-image_${name}`);
+
+  if (value) {
+    let matches: RegExpMatchArray;
+    const gximage = gxImageConstructor(name);
+
+    while ((matches = value.match(URL_REGEX))) {
+      gximage.densitySet.push({
+        uri: normalizeUri(matches[1] ? matches[2].slice(0, -1) : matches[2]),
+        density: matches[3] ? parseFloat(matches[3]) : 1
+      });
+
+      value = value.slice(matches[0].length);
+    }
+
+    if (gximage.densitySet.length > 0) {
+      gximage.uri = gximage.densitySet.reduce((previousValue, currentValue) => {
+        return previousValue.density === 1 ||
+          previousValue.density < currentValue.density
+          ? previousValue
+          : currentValue;
+      }).uri;
+    }
+
+    return gximage;
+  }
+}
+
+export const fromGxImageToURL = (
+  image: any,
+  Settings: any,
+  gxImageConstructor: (name: string) => any
+): string => {
+  let url = "";
+  if (image.id) {
+    url = getImage(image.id, gxImageConstructor).uri;
+  } else {
+    url = image.uri;
+  }
+
+  if (!url) {
+    return "";
+  }
+
+  const baseUrl = Settings.WEBAPP_BASE;
+  const urlLower = url.toLowerCase();
+  if (urlLower.startsWith("assets/")) {
+    // Relative URL to local assets
+    return url;
+  } else if (
+    urlLower.startsWith("http://") ||
+    urlLower.startsWith("https://") ||
+    urlLower.startsWith("blob:") ||
+    urlLower.startsWith("file:") ||
+    urlLower.startsWith("data:")
+  ) {
+    // Absolute URL
+    return url;
+  } else if (urlLower.startsWith(Settings.BASE_PATH.toLowerCase())) {
+    // Host relative URL
+    return baseUrl + url.substring(Settings.BASE_PATH.length);
+  } else {
+    return baseUrl + url;
+  }
+};

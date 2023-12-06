9918be9852a888207a00064aad3c9bcf0586af33
Nicolás Cámera <ncamera@genexus.com>
2023-12-05 23:45:26 -0300

    Simplify drag-bar implementation

--- src/components/drag-bar/drag-bar.tsx
+++ src/components/drag-bar/drag-bar.tsx
@@ -31,7 +31,7 @@ export class DragBar implements ChComponent {
   private mainContainerRef: HTMLDivElement;
   private startComponentRef: HTMLDivElement;
 
-  @Element() element: HTMLChDragBarElement;
+  @Element() el: HTMLChDragBarElement;
 
   /**
    * Determine the language direction.
@@ -48,18 +48,6 @@ export class DragBar implements ChComponent {
    * A CSS class to set as the `ch-next-drag-bar` element class.
    */
   @Prop() readonly cssClass: string;
-
-  /**
-   * Specifies the bar item src.
-   * If defined, it will set an image to replace the default bar item.
-   */
-  @Prop() readonly barItemSrc: string = "";
-
-  /**
-   * If `true` an item at the middle of the bar will be displayed to give more
-   * context about the resize action
-   */
-  @Prop() readonly showBarItem: boolean = true;
 
   /**
    * Specifies the initial width of the start component
@@ -98,7 +86,7 @@ export class DragBar implements ChComponent {
         startComponentWidth = 100 - startComponentWidth;
       }
 
-      this.startComponentRef.style.setProperty(
+      this.el.style.setProperty(
         START_COMPONENT_WIDTH,
         `${this.keepValueInBetween(startComponentWidth)}%`
       );
@@ -206,46 +194,7 @@ export class DragBar implements ChComponent {
             class="bar"
             part="bar"
             ref={el => (this.barRef = el)}
-          >
-            {this.showBarItem && (
-              <div class="bar-item" part="bar-item">
-                {this.barItemSrc ? (
-                  <img
-                    aria-hidden="true"
-                    class="bar-item-src"
-                    part="bar-item-src"
-                    alt=""
-                    src={this.barItemSrc}
-                  />
-                ) : (
-                  <svg
-                    aria-hidden="true"
-                    class={{
-                      "bar-item-src": true,
-                      "bar-item-src--rtl": this.isRTLDirection
-                    }}
-                    part="bar-item-src"
-                    viewBox="0 0 12 57"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M10.1899 4.59531H3.82598L6.3645 2.0568L4.95028 0.642585L0.000535564 5.59233L4.95028 10.5421L6.3645 9.12787L3.82598 6.58935H10.1899V4.59531Z"
-                      fill="#969BA0"
-                    />
-                    <path
-                      d="M0 37.1855V34.6141H2V37.1855H0ZM4 37.1855V34.6141H6V37.1855H4ZM8 37.1855V34.6141H10V37.1855H8ZM0 32.0427V29.4713H2V32.0427H0ZM4 32.0427V29.4713H6V32.0427H4ZM8 32.0427V29.4713H10V32.0427H8ZM0 26.8998V24.3284H2V26.8998H0ZM4 26.8998V24.3284H6V26.8998H4ZM8 26.8998V24.3284H10V26.8998H8ZM0 21.757V19.1855H2V21.757H0ZM4 21.757V19.1855H6V21.757H4ZM8 21.757V19.1855H10V21.757H8Z"
-                      transform="translate(1)"
-                      fill="#6E7277"
-                    />
-                    <path
-                      d="M4.823 47.2443L6.23722 45.8301L11.187 50.7798L6.23722 55.7296L4.823 54.3154L7.36152 51.7769H0.997556V49.7828L7.36152 49.7828L4.823 47.2443Z"
-                      fill="#969BA0"
-                    />
-                  </svg>
-                )}
-              </div>
-            )}
-          </div>
+          ></div>
 
           <div class="end-component" part="end-component">
             <slot name="end-component" />

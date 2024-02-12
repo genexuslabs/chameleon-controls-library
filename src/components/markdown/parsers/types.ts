f779b029ea03fa06939009a91f5576a3f8fc6282
Nicolás Cámera <ncamera@genexus.com>
2024-02-12 18:55:52 -0300

    Add types

---
+++ src/components/markdown/parsers/types.ts
@@ -0,0 +1,35 @@
+import {
+  Break,
+  Code,
+  Definition,
+  FootnoteReference,
+  Html,
+  Image,
+  ImageReference,
+  InlineCode,
+  RootContent,
+  RootContentMap,
+  Text,
+  ThematicBreak,
+  Yaml
+} from "mdast";
+
+export type ElementsWithoutChildren = Exclude<
+  RootContent,
+  | Break
+  | Code
+  | Definition
+  | FootnoteReference
+  | Html
+  | Image
+  | ImageReference
+  | InlineCode
+  | Text
+  | ThematicBreak
+  | Yaml
+>;
+
+export type ElementsWithoutCustomRender = Omit<
+  RootContentMap,
+  "tableCell" | "tableRow"
+>;

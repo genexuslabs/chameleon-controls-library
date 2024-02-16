1c8554d5a1638b529761ade038adcb405940dbb0
Nicolás Cámera <ncamera@genexus.com>
2024-02-16 12:43:57 -0300

    Update documentation

--- src/components.d.ts
+++ src/components.d.ts
@@ -998,6 +998,12 @@ export namespace Components {
     /**
      * A control to render markdown syntax. It supports GitHub Flavored Markdown
      * (GFM) and code highlighting.
+     *  - It parses the incoming markdown to [mdast](https://github.com/syntax-tree/mdast) using [micromark](https://github.com/micromark/micromark) via [mdast-util-from-markdown](https://github.com/syntax-tree/mdast-util-from-markdown).
+     * - After that, it implements a reactivity layer by implementing its own render for the mdast. With this, changes to the input markdown only update the portion of the DOM that changes.
+     * - It supports Github Flavored Markdown (GFM) by using [mdast-util-gfm](https://github.com/syntax-tree/mdast-util-gfm) and [micromark-extension-gfm](https://github.com/micromark/micromark-extension-gfm).
+     * - It supports code highlight by parsing the incomming code of the markdown to [hast](https://github.com/micromark/micromark-extension-gfm) using [lowlight](lowlight). After that, it implements a reactivity layer by implementing its own render for the hast.
+     * - It also supports all programming languages from [highlight.js](https://github.com/highlightjs/highlight.js).
+     * - When the code highlighting is needed at runtime, the control will load on demand the code parser and the programming language needed to parse the code.
      */
     interface ChMarkdown {
         /**
@@ -2800,6 +2806,12 @@ declare global {
     /**
      * A control to render markdown syntax. It supports GitHub Flavored Markdown
      * (GFM) and code highlighting.
+     *  - It parses the incoming markdown to [mdast](https://github.com/syntax-tree/mdast) using [micromark](https://github.com/micromark/micromark) via [mdast-util-from-markdown](https://github.com/syntax-tree/mdast-util-from-markdown).
+     * - After that, it implements a reactivity layer by implementing its own render for the mdast. With this, changes to the input markdown only update the portion of the DOM that changes.
+     * - It supports Github Flavored Markdown (GFM) by using [mdast-util-gfm](https://github.com/syntax-tree/mdast-util-gfm) and [micromark-extension-gfm](https://github.com/micromark/micromark-extension-gfm).
+     * - It supports code highlight by parsing the incomming code of the markdown to [hast](https://github.com/micromark/micromark-extension-gfm) using [lowlight](lowlight). After that, it implements a reactivity layer by implementing its own render for the hast.
+     * - It also supports all programming languages from [highlight.js](https://github.com/highlightjs/highlight.js).
+     * - When the code highlighting is needed at runtime, the control will load on demand the code parser and the programming language needed to parse the code.
      */
     interface HTMLChMarkdownElement extends Components.ChMarkdown, HTMLStencilElement {
     }
@@ -4333,6 +4345,12 @@ declare namespace LocalJSX {
     /**
      * A control to render markdown syntax. It supports GitHub Flavored Markdown
      * (GFM) and code highlighting.
+     *  - It parses the incoming markdown to [mdast](https://github.com/syntax-tree/mdast) using [micromark](https://github.com/micromark/micromark) via [mdast-util-from-markdown](https://github.com/syntax-tree/mdast-util-from-markdown).
+     * - After that, it implements a reactivity layer by implementing its own render for the mdast. With this, changes to the input markdown only update the portion of the DOM that changes.
+     * - It supports Github Flavored Markdown (GFM) by using [mdast-util-gfm](https://github.com/syntax-tree/mdast-util-gfm) and [micromark-extension-gfm](https://github.com/micromark/micromark-extension-gfm).
+     * - It supports code highlight by parsing the incomming code of the markdown to [hast](https://github.com/micromark/micromark-extension-gfm) using [lowlight](lowlight). After that, it implements a reactivity layer by implementing its own render for the hast.
+     * - It also supports all programming languages from [highlight.js](https://github.com/highlightjs/highlight.js).
+     * - When the code highlighting is needed at runtime, the control will load on demand the code parser and the programming language needed to parse the code.
      */
     interface ChMarkdown {
         /**
@@ -5732,6 +5750,12 @@ declare module "@stencil/core" {
             /**
              * A control to render markdown syntax. It supports GitHub Flavored Markdown
              * (GFM) and code highlighting.
+             *  - It parses the incoming markdown to [mdast](https://github.com/syntax-tree/mdast) using [micromark](https://github.com/micromark/micromark) via [mdast-util-from-markdown](https://github.com/syntax-tree/mdast-util-from-markdown).
+             * - After that, it implements a reactivity layer by implementing its own render for the mdast. With this, changes to the input markdown only update the portion of the DOM that changes.
+             * - It supports Github Flavored Markdown (GFM) by using [mdast-util-gfm](https://github.com/syntax-tree/mdast-util-gfm) and [micromark-extension-gfm](https://github.com/micromark/micromark-extension-gfm).
+             * - It supports code highlight by parsing the incomming code of the markdown to [hast](https://github.com/micromark/micromark-extension-gfm) using [lowlight](lowlight). After that, it implements a reactivity layer by implementing its own render for the hast.
+             * - It also supports all programming languages from [highlight.js](https://github.com/highlightjs/highlight.js).
+             * - When the code highlighting is needed at runtime, the control will load on demand the code parser and the programming language needed to parse the code.
              */
             "ch-markdown": LocalJSX.ChMarkdown & JSXBase.HTMLAttributes<HTMLChMarkdownElement>;
             "ch-next-data-modeling": LocalJSX.ChNextDataModeling & JSXBase.HTMLAttributes<HTMLChNextDataModelingElement>;
--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -6,6 +6,17 @@ import { MarkdownCodeRender } from "./parsers/types";
 /**
  * A control to render markdown syntax. It supports GitHub Flavored Markdown
  * (GFM) and code highlighting.
+ *  - It parses the incoming markdown to [mdast](https://github.com/syntax-tree/mdast) using [micromark](https://github.com/micromark/micromark) via [mdast-util-from-markdown](https://github.com/syntax-tree/mdast-util-from-markdown).
+ *
+ * - After that, it implements a reactivity layer by implementing its own render for the mdast. With this, changes to the input markdown only update the portion of the DOM that changes.
+ *
+ * - It supports Github Flavored Markdown (GFM) by using [mdast-util-gfm](https://github.com/syntax-tree/mdast-util-gfm) and [micromark-extension-gfm](https://github.com/micromark/micromark-extension-gfm).
+ *
+ * - It supports code highlight by parsing the incomming code of the markdown to [hast](https://github.com/micromark/micromark-extension-gfm) using [lowlight](lowlight). After that, it implements a reactivity layer by implementing its own render for the hast.
+ *
+ * - It also supports all programming languages from [highlight.js](https://github.com/highlightjs/highlight.js).
+ *
+ * - When the code highlighting is needed at runtime, the control will load on demand the code parser and the programming language needed to parse the code.
  */
 @Component({
   shadow: false,

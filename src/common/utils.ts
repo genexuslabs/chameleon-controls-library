16dec59570b01f7874188287b6309df868ecb918
Nicolás Cámera <ncamera@genexus.com>
2024-02-19 10:44:04 -0300

    Add spec tests
    Added a WA to fix the undefined "new Image()" reference when running spec tests.

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -69,7 +69,13 @@ export const tokenMap = (tokens: { [key: string]: boolean }) =>
  */
 export const inBetween = (x: number, y: number, z: number) => x <= y && y <= z;
 
-const resetDragImage = new Image();
+let resetDragImage;
+
+/* @__PURE__ */ if (!window.Image) {
+  resetDragImage = "test";
+}
+
+resetDragImage ??= new Image();
 export const removeDragImage = (event: DragEvent) =>
   event.dataTransfer.setDragImage(resetDragImage, 0, 0);
 

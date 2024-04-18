f54f0733280525e4c8677194060cc6f007d467b6
Nicolás Cámera <ncamera@genexus.com>
2024-04-18 13:56:31 -0300

    Ignore ESLint issues in deprecated controls

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     project: "./tsconfig.json",
     sourceType: "module" // Allows for the use of imports
   },
+  ignorePatterns: ["src/deprecated-components/*"],
   rules: {
     // - - - - - - - - - - - -
     // ESLint

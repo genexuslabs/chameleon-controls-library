3a330c124aa73f218930024ee99a9a30a13c469f
Nicolás Cámera <ncamera@genexus.com>
2024-04-18 12:23:04 -0300

    Make ESLint stricter

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2022, // Allows for the parsing of modern ECMAScript features
     project: "./tsconfig.json",
     sourceType: "module" // Allows for the use of imports
   },
@@ -32,12 +32,12 @@ module.exports = {
     "no-debugger": "error", // Error when using debugger;
     "no-duplicate-case": "error", // This rule disallows duplicate test expressions in case clauses of switch statements
     "no-empty": "error", // Disallow empty block statements
-    "no-lonely-if": "warn", // Disallow if statements as the only statement in else blocks
+    "no-lonely-if": "error", // Disallow if statements as the only statement in else blocks
     "no-multi-assign": "error", // Disallow use of chained assignment expressions
-    "no-nested-ternary": "warn", // Warns the use of nested ternary expressions
+    "no-nested-ternary": "error", // Errors when using nested ternary expressions
     "no-sequences": "error", // Disallow comma operators
     "no-undef": "off", // Allows defining undefined variables
-    "no-unneeded-ternary": "warn", // Disallow ternary operators when simpler alternatives exist
+    "no-unneeded-ternary": "error", // Disallow ternary operators when simpler alternatives exist
     "no-useless-return": "error", // Disallow redundant return statements
     "spaced-comment": ["error", "always", { exceptions: ["-", "+", "/"] }], // Enforce consistent spacing after the // or /* in a comment
 
@@ -66,8 +66,8 @@ module.exports = {
     // - - - - - - - - - - - -
     // StencilJS
     // - - - - - - - - - - - -
-    "@stencil-community/async-methods": "warn", // This rule catches Stencil public methods that are not async
-    "@stencil-community/decorators-context": "warn", // This rule catches Stencil decorators in bad locations
+    "@stencil-community/async-methods": "error", // This rule catches Stencil public methods that are not async
+    "@stencil-community/decorators-context": "error", // This rule catches Stencil decorators in bad locations
     "@stencil-community/decorators-style": [
       "warn",
       {
@@ -80,18 +80,18 @@ module.exports = {
       }
     ],
     "@stencil-community/ban-exported-const-enums": "warn", // Exported const enums are not allowed
-    "@stencil-community/element-type": "warn", // This rule catches Stencil Element decorator have the correct type
-    "@stencil-community/methods-must-be-public": "warn", // This rule catches Stencil Methods marked as private or protected
-    "@stencil-community/no-unused-watch": "warn", // This rule catches Stencil Watchs with non existing Props or States
+    "@stencil-community/element-type": "error", // This rule catches Stencil Element decorator have the correct type
+    "@stencil-community/methods-must-be-public": "error", // This rule catches Stencil Methods marked as private or protected
+    "@stencil-community/no-unused-watch": "error", // This rule catches Stencil Watchs with non existing Props or States
     "@stencil-community/own-methods-must-be-private": "warn", // This rule catches own class methods marked as public
     "@stencil-community/own-props-must-be-private": "warn", // This rule catches own class properties marked as public
     "@stencil-community/prefer-vdom-listener": "warn", // This rule catches Stencil Listen with vdom events
-    "@stencil-community/props-must-be-public": "warn", // This rule catches Stencil Props marked as private or protected
-    "@stencil-community/props-must-be-readonly": "warn", // This rule catches Stencil Props marked as non readonly, excluding mutable ones
-    "@stencil-community/required-jsdoc": "warn", // This rule catches Stencil Props, Methods and Events to define jsdoc
-    "@stencil-community/required-prefix": ["warn", ["ch-"]], // Ensures that a Component's tag use the "ch-" prefix.
+    "@stencil-community/props-must-be-public": "error", // This rule catches Stencil Props marked as private or protected
+    "@stencil-community/props-must-be-readonly": "error", // This rule catches Stencil Props marked as non readonly, excluding mutable ones
+    "@stencil-community/required-jsdoc": "error", // This rule catches Stencil Props, Methods and Events to define jsdoc
+    "@stencil-community/required-prefix": ["error", ["ch-"]], // Ensures that a Component's tag use the "ch-" prefix.
     "@stencil-community/reserved-member-names": "warn", // Ensures that any of reserved global HTML attribute names are used as @Prop or @Method
-    "@stencil-community/single-export": "warn", // This rule catches modules that expose more than just the Stencil Component itself
+    "@stencil-community/single-export": "error", // This rule catches modules that expose more than just the Stencil Component itself
     "@stencil-community/strict-boolean-conditions": "off",
 
     // WA to fix false positive errors

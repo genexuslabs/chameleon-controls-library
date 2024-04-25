f197a4c105136a4335997c7bdcc780fca6fe0daf
Nicolás Cámera <81186472+ncamera@users.noreply.github.com>
2024-04-25 14:40:17 -0300

    Improvements for `ch-combo-box` control (#288)
    * Add initial support for ComboBox filters
    
    Breaking change: The "ComboBoxItem" type was renamed to "ComboBoxItemModel"
    
    * Update combo-box.html
    
    * Improve filter implementation
    
    * Update combo-box.html
    
    * Improve the implementation of filters
    
    * Improve filters and add support to collapse groups
    
    * Update combo-box.html
    
    * Improve keyboard navigation with filters
    
    * Refactoring for keyboard implementation
    
    * Improve focus implementation when clicking labels
    
    * Avoid opening the popover when pressing the ARROW_LEFT or ARROW_RIGHT keys
    
    * Add support to expand and collapse groups
    
    * Improve customization of groups and items
    
    * Fix filter condition
    
    * Add ellipsis when the text overflows
    
    * Simplify input positioning
    
    * Simplify grid-template
    
    * Simplify picker's alignment
    
    * Improve "readonly" and "disabled" states
    
    * Improve popover alignment
    
    * Update combo-box.html
    
    * Fix for "--ch-combo-box-separation" support
    
    * Improve custom var documentation
    
    * Update readme.md
    
    * Update components.d.ts

---
+++ src/components/combobox/helpers.ts
@@ -0,0 +1,100 @@
+import {
+  ComboBoxFilterInfo,
+  ComboBoxFilterOptions,
+  ComboBoxFilterType,
+  ComboBoxItemGroup,
+  ComboBoxItemModel
+} from "./types";
+
+const filterWithCase = (
+  stringToFilter: string,
+  filter: string,
+  matchCase?: boolean
+) =>
+  matchCase
+    ? stringToFilter.includes(filter)
+    : stringToFilter.toLowerCase().includes(filter.toLowerCase());
+
+const filterWithString = (
+  stringToFilter: string,
+  filter: string,
+  filterOptions: ComboBoxFilterOptions
+) =>
+  filterOptions?.regularExpression
+    ? stringToFilter.match(filter) !== null
+    : filterWithCase(stringToFilter, filter, filterOptions?.matchCase);
+
+const filterDictionary: {
+  [key in ComboBoxFilterType]: (
+    item: ComboBoxItemModel,
+    filterInfo: ComboBoxFilterInfo
+  ) => boolean;
+} = {
+  caption: (item, filterInfo) =>
+    filterInfo.filter
+      ? filterWithString(
+          item.caption ?? "",
+          filterInfo.filter,
+          filterInfo.filterOptions
+        )
+      : true,
+
+  list: (item, filterInfo) => filterInfo.filterSet.has(item.value),
+
+  value: (item, filterInfo) =>
+    filterInfo.filter
+      ? filterWithString(
+          item.value,
+          filterInfo.filter,
+          filterInfo.filterOptions
+        )
+      : true,
+
+  none: () => true
+};
+
+const computeFilter = (
+  filterType: ComboBoxFilterType,
+  item: ComboBoxItemModel,
+  filterInfo: ComboBoxFilterInfo
+): boolean =>
+  filterInfo.filterOptions?.hideMatchesAndShowNonMatches === true
+    ? !filterDictionary[filterType](item, filterInfo)
+    : filterDictionary[filterType](item, filterInfo);
+
+export const filterSubModel = (
+  item: ComboBoxItemModel,
+  filterType: ComboBoxFilterType,
+  filterInfo: ComboBoxFilterInfo,
+  displayedValues: Set<string>
+): boolean => {
+  // Check if a subitem is rendered
+  let aSubItemIsRendered = false;
+  const itemSubGroup = (item as ComboBoxItemGroup).items;
+
+  if (itemSubGroup != null) {
+    for (let index = 0; index < itemSubGroup.length; index++) {
+      const itemLeaf = itemSubGroup[index];
+      const itemSatisfiesFilter = filterSubModel(
+        itemLeaf,
+        filterType,
+        filterInfo,
+        displayedValues
+      );
+
+      aSubItemIsRendered ||= itemSatisfiesFilter;
+    }
+  }
+
+  // The current item is rendered if it satisfies the filter condition or a
+  // subitem exists that needs to be rendered
+  const satisfiesFilter =
+    aSubItemIsRendered || computeFilter(filterType, item, filterInfo);
+
+  // Update selected and checkbox items
+  if (satisfiesFilter) {
+    displayedValues.add(item.value);
+  }
+
+  return satisfiesFilter;
+};

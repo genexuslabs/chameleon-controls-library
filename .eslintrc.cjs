185c450e4e88b7d5f9b88ac0a184a9a9ee766ae3
Nicolás Cámera <ncamera@genexus.com>
2024-04-25 11:09:21 -0300

    Migrate ESLint to CJS and disable local plugin as a WA to re-enable ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  plugins: ["@typescript-eslint", "local"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:@stencil-community/recommended", // Enables @stencil-community/eslint-plugin.
@@ -102,14 +102,14 @@ module.exports = {
 
     // WA to fix false positive errors
     "@stencil-community/strict-mutable": "warn" // This rule catches Stencil Prop marked as mutable but not changing value in code
-  },
-  overrides: [
-    {
-      files: ["**/*.tsx"],
-      rules: {
-        "local/jsx-uses-my-pragma": "error", // These are needed to avoid getting a not used error with imports used in JSX
-        "local/jsx-uses-vars": "error"
-      }
-    }
-  ]
+  }
+  // overrides: [
+  //   {
+  //     files: ["**/*.tsx"],
+  //     rules: {
+  //       "local/jsx-uses-my-pragma": "error", // These are needed to avoid getting a not used error with imports used in JSX
+  //       "local/jsx-uses-vars": "error"
+  //     }
+  //   }
+  // ]
 };

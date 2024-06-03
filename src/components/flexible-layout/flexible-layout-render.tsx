050c18244beac33eed96b65089c1e969db8cb08e
Nicolás Cámera <ncamera@genexus.com>
2024-06-03 12:07:59 -0300

    Add "updateSelectedWidget" method in the Flexible Layout control

--- src/components/flexible-layout/flexible-layout-render.tsx
+++ src/components/flexible-layout/flexible-layout-render.tsx
@@ -319,6 +319,37 @@ export class ChFlexibleLayoutRender {
     );
   }
 
+  /**
+   * Update the selected widget from a `"tabbed"` type leaf.
+   * Only works if the parent leaf is `"tabbed"` type.
+   */
+  @Method()
+  async updateSelectedWidget(
+    parentLeafId: string,
+    newSelectedWidgetId: string
+  ) {
+    const widgetUIModel = this.#widgetsInfo.get(newSelectedWidgetId);
+
+    if (!widgetUIModel || widgetUIModel.parentLeafId !== parentLeafId) {
+      return;
+    }
+    const parentLeafInfo = this.#getLeafInfo(widgetUIModel.parentLeafId);
+
+    if (
+      parentLeafInfo.type === "single-content" ||
+      parentLeafInfo.selectedWidgetId === newSelectedWidgetId
+    ) {
+      return;
+    }
+
+    // Select the new item
+    this.#updateSelectedWidget(parentLeafInfo, widgetUIModel.info);
+
+    // Queue re-renders
+    forceUpdate(this);
+    forceUpdate(this.#flexibleLayoutRef);
+  }
+
   #updateFlexibleModels = (layout: FlexibleLayoutModel) => {
     // Empty layout
     if (layout == null) {

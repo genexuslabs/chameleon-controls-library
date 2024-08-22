836d1262607aa567e86e107b59655582318db67c
Nicolás Cámera <ncamera@genexus.com>
2024-08-22 16:11:20 -0300

    Improve tests

--- src/components/combo-box/tests/suggest-behavior.e2e.ts
+++ src/components/combo-box/tests/suggest-behavior.e2e.ts
@@ -1,12 +1,22 @@
-import { newE2EPage } from "@stencil/core/testing";
+import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
 
 describe("[ch-combo-box-render][behavior][suggest]", () => {
-  it("the picker should not have a visible size by default", async () => {
-    const page = await newE2EPage();
-    await page.setContent(
-      `<ch-combo-box-render filter-type="caption"></ch-combo-box-render>`
-    );
+  let page: E2EPage;
+  let comboBoxRef: E2EElement;
 
+  beforeEach(async () => {
+    page = await newE2EPage({
+      html: `<ch-combo-box-render suggest></ch-combo-box-render>`
+    });
+    comboBoxRef = await page.find("ch-combo-box-render");
+  });
+
+  it('should not have an aria-hidden="true" attribute the mask', async () => {
+    const maskRef = comboBoxRef.shadowRoot.querySelector(".mask");
+    expect(maskRef).not.toHaveAttribute("aria-hidden");
+  });
+
+  it("the picker should not have a visible size by default", async () => {
     // This does not computed the custom vars: comboBoxRef.getComputedStyle()
     const pickerSize = await page.evaluate(() =>
       getComputedStyle(

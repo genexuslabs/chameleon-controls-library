540c8cd3a296c8385fe5d41ff56f34e52b013636
Nicolás Cámera <ncamera@genexus.com>
2024-08-30 22:15:28 -0300

    Refactor ch-tooltip implementation
     - Added support for Shadow DOM.
    
     - Replaced the ch-window component with the ch-popover.
    
     - Reduced the event listeners used to implement the component.
    
     - Added support for "standalone mode".
    
     - Improved accesibility.

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -1,139 +1,204 @@
-import { Component, Host, h, Prop, State } from "@stencil/core";
-import { Component as ChComponent } from "../../common/interfaces";
-import { focusComposedPath } from "../common/helpers";
+import {
+  Component,
+  ComponentInterface,
+  Element,
+  Host,
+  h,
+  Prop,
+  State,
+  Watch
+} from "@stencil/core";
 import { ChPopoverAlign } from "../popover/types";
 
-export type TooltipAlign =
-  | "OutsideStart"
-  | "InsideStart"
-  | "Center"
-  | "InsideEnd"
-  | "OutsideEnd";
+const LISTENER_CONFIG = {
+  once: true,
+  passive: true
+} as const satisfies AddEventListenerOptions;
 
-const mapTooltipAlignToChWindowAlign: {
-  [key in TooltipAlign]: ChPopoverAlign;
-} = {
-  OutsideStart: "outside-start",
-  InsideStart: "inside-start",
-  Center: "center",
-  InsideEnd: "inside-end",
-  OutsideEnd: "outside-end"
-};
-
-const EXPORT_PARTS = "window:section,mask";
+let autoId = 0;
 
 @Component({
   tag: "ch-tooltip",
   styleUrl: "tooltip.scss",
-  shadow: false
+  shadow: true
 })
-export class ChTooltip implements ChComponent {
-  /** The container element for ch-window.
-   */
-  private container!: HTMLDivElement;
-  /** Determines if the window and the tooltip is hidden or visible.
-   * Inherited from ch-window.
-   */
-  @State() hidden = true;
+export class ChTooltip implements ComponentInterface {
+  #tooltipId: string;
+  #timeoutId: NodeJS.Timeout;
+
+  #addListenersForTheActionElement = true;
+  #actualActionElement: HTMLButtonElement;
+
+  // Refs
+  #innerActionRef: HTMLButtonElement | undefined;
+
+  @Element() el!: HTMLChTooltipElement;
+
   /**
-   * Specifies the tooltip description.
+   * Specifies if the popover is visible.
    */
-  @Prop() readonly tooltipId: string = "Tooltip";
+  @State() visible = false;
+  @Watch("visible")
+  visibleChanged(isVisible: boolean) {
+    if (isVisible) {
+      this.#removeDisplayListeners();
+      this.#addListenersToHidePopover();
+    } else {
+      this.#removeHideListeners();
+      this.#addListenersToDisplayPopover();
+    }
+  }
+
   /**
-   * Specifies the delay (in ms)
-   * for the tooltip to be displayed.
+   * Specifies a reference for the action that opens the tooltip.
+   *
+   * If `undefined`, a button on the tooltip will be rendered and the slot
+   * "action" can be used to display the content of the action.
+   *
+   * If `null`, the parentElement will be used as the action reference.
+   */
+  @Prop() readonly actionElement?: HTMLButtonElement | undefined | null;
+  @Watch("actionElement")
+  actionElementChanged() {
+    this.#removeAllListeners();
+    this.#addListenersForTheActionElement = true;
+  }
+
+  /**
+   * Specifies the block alignment of the window.
+   */
+  @Prop() readonly blockAlign: ChPopoverAlign = "outside-end";
+
+  /**
+   * Specifies the delay (in ms) for the tooltip to be displayed.
    */
   @Prop() readonly delay: number = 100;
-  /**
-   * Specifies the position of the tooltip relative to
-   * the container element.
-   */
-  @Prop() readonly position:
-    | "OutsideStart_OutsideStart"
-    | "InsideStart_OutsideStart"
-    | "Center_OutsideStart"
-    | "InsideEnd_OutsideStart"
-    | "OutsideEnd_OutsideStart"
-    | "OutsideStart_InsideStart"
-    | "OutsideEnd_InsideStart"
-    | "OutsideStart_Center"
-    | "OutsideEnd_Center"
-    | "OutsideStart_InsideEnd"
-    | "OutsideEnd_InsideEnd"
-    | "OutsideStart_OutsideEnd"
-    | "InsideStart_OutsideEnd"
-    | "Center_OutsideEnd"
-    | "InsideEnd_OutsideEnd"
-    | "OutsideEnd_OutsideEnd" = "OutsideStart_Center";
 
-  private showWithDelay = () => {
-    setTimeout(() => {
-      this.hidden = false;
+  /**
+   * Specifies the inline alignment of the window.
+   */
+  @Prop() readonly inlineAlign: ChPopoverAlign = "center";
+
+  #handleEnter = () => {
+    clearTimeout(this.#timeoutId);
+    this.#timeoutId = setTimeout(() => {
+      this.visible = true;
     }, this.delay);
   };
 
-  private handleEnter = () => {
-    if (!this.hidden) {
-      return;
-    }
-    this.showWithDelay();
+  #handleLeave = () => {
+    clearTimeout(this.#timeoutId);
+    this.visible = false;
   };
 
-  private handleLeave = () => {
-    if (this.container === focusComposedPath()[0]) {
-      this.hidden = false;
-    }
-    this.hidden = true;
-  };
-
-  private addListeners = () => {
-    this.container.addEventListener("focus", this.handleEnter);
-    this.container.addEventListener("focusout", this.handleLeave);
-    this.container.addEventListener("mouseenter", this.handleEnter);
-    this.container.addEventListener("mouseleave", this.handleLeave);
-  };
-
-  private removeListeners = () => {
-    this.container.removeEventListener("focus", this.handleEnter);
-    this.container.removeEventListener("focusout", this.handleLeave);
-    this.container.removeEventListener("mouseenter", this.handleEnter);
-    this.container.removeEventListener("mouseleave", this.handleLeave);
-  };
-
-  componentDidLoad() {
-    this.container = document.querySelector(
-      '[slot="container"] > :first-child'
+  #addListenersToDisplayPopover = () => {
+    this.#actualActionElement.addEventListener(
+      "focus",
+      this.#handleEnter,
+      LISTENER_CONFIG
     );
-    this.addListeners();
+    this.#actualActionElement.addEventListener(
+      "mouseenter",
+      this.#handleEnter,
+      LISTENER_CONFIG
+    );
+  };
+
+  #addListenersToHidePopover = () => {
+    this.#actualActionElement.addEventListener(
+      "focusout",
+      this.#handleLeave,
+      LISTENER_CONFIG
+    );
+    this.#actualActionElement.addEventListener(
+      "mouseleave",
+      this.#handleLeave,
+      LISTENER_CONFIG
+    );
+  };
+
+  #removeDisplayListeners = () => {
+    this.#actualActionElement?.removeEventListener("focus", this.#handleEnter);
+    this.#actualActionElement?.removeEventListener(
+      "mouseenter",
+      this.#handleEnter
+    );
+  };
+
+  #removeHideListeners = () => {
+    this.#actualActionElement?.removeEventListener(
+      "focusout",
+      this.#handleLeave
+    );
+    this.#actualActionElement?.removeEventListener(
+      "mouseleave",
+      this.#handleLeave
+    );
+  };
+
+  #getActionElement = (): HTMLButtonElement => {
+    if (this.actionElement === null) {
+      return this.el.parentElement as HTMLButtonElement;
+    }
+
+    return this.actionElement ?? this.#innerActionRef;
+  };
+
+  #removeAllListeners = () => {
+    this.#removeDisplayListeners();
+    this.#removeHideListeners();
+  };
+
+  connectedCallback() {
+    this.#tooltipId ??= `ch-tooltip-${autoId++}`;
+  }
+
+  componentDidRender() {
+    if (this.#addListenersForTheActionElement) {
+      this.#addListenersForTheActionElement = false;
+      this.#actualActionElement = this.#getActionElement();
+
+      this.#addListenersToDisplayPopover();
+    }
   }
 
   disconnectedCallback() {
-    this.removeListeners();
+    this.#removeAllListeners();
   }
 
   render() {
-    const aligns = this.position.split("_");
-    const alignX = aligns[0] as TooltipAlign;
-    const alignY = aligns[1] as TooltipAlign;
+    const actionInsideShadow = this.actionElement === undefined;
 
     return (
-      <Host>
-        <slot name="container"></slot>
-        <ch-window
-          exportparts={EXPORT_PARTS}
-          show-header={false}
-          show-footer={false}
-          modal={false}
-          close-on-escape
-          hidden={this.hidden}
-          container={this.container}
-          xAlign={mapTooltipAlignToChWindowAlign[alignX]}
-          yAlign={mapTooltipAlignToChWindowAlign[alignY]}
+      <Host
+        id={!actionInsideShadow ? this.#tooltipId : undefined}
+        role={!actionInsideShadow ? "tooltip" : undefined}
+      >
+        {actionInsideShadow && (
+          <button
+            aria-describedby={this.#tooltipId}
+            part="action"
+            type="button"
+            ref={el => (this.#innerActionRef = el)}
+          >
+            <slot name="action"></slot>
+          </button>
+        )}
+
+        <ch-popover
+          id={actionInsideShadow ? this.#tooltipId : undefined}
+          role={actionInsideShadow ? "tooltip" : undefined}
+          part="window"
+          // Don't use #actualActionElement. On the first render is not defined
+          actionElement={this.#getActionElement()}
+          blockAlign={this.blockAlign}
+          closeOnClickOutside
+          hidden={!this.visible}
+          inlineAlign={this.inlineAlign}
+          mode="manual"
         >
-          <div role="tooltip" class="tooltip-content" id={this.tooltipId}>
-            <slot name="content"></slot>
-          </div>
-        </ch-window>
+          <slot />
+        </ch-popover>
       </Host>
     );
   }

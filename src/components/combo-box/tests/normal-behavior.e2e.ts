3326390ade9f51aaa5d2b671f41171e28ad4748f
Nicolás Cámera <ncamera@genexus.com>
2024-09-26 00:17:27 -0300

    Add e2e tests

--- src/components/combo-box/tests/normal-behavior.e2e.ts
+++ src/components/combo-box/tests/normal-behavior.e2e.ts
@@ -1,5 +1,6 @@
 import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
 import { DEFAULT_DECORATIVE_SIZE } from "../../../testing/constants.e2e";
+import { simpleModelComboBox1 } from "../../../showcase/assets/components/combo-box/models";
 
 describe("[ch-combo-box-render][behavior]", () => {
   let page: E2EPage;
@@ -7,7 +8,8 @@ describe("[ch-combo-box-render][behavior]", () => {
 
   beforeEach(async () => {
     page = await newE2EPage({
-      html: `<ch-combo-box-render></ch-combo-box-render>`,
+      html: `<button>Dummy button</button>
+      <ch-combo-box-render></ch-combo-box-render>`,
       failOnConsoleError: true
     });
     comboBoxRef = await page.find("ch-combo-box-render");
@@ -27,5 +29,30 @@ describe("[ch-combo-box-render][behavior]", () => {
     );
 
     expect(pickerSize).toBe(DEFAULT_DECORATIVE_SIZE);
+  });
+
+  it("should not render items if the popover is not displayed", async () => {
+    let popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeNull();
+
+    await comboBoxRef.setProperty("model", simpleModelComboBox1);
+    await page.waitForChanges();
+    popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeNull();
+  });
+
+  it("should destroy the items after the popover is closed", async () => {
+    await comboBoxRef.setProperty("model", simpleModelComboBox1);
+    await page.waitForChanges();
+    await page.click("ch-combo-box-render");
+    await page.waitForChanges();
+
+    let popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeDefined();
+
+    await page.click("button");
+    await page.waitForChanges();
+    popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeNull();
   });
 });
--- src/components/combo-box/tests/suggest-behavior.e2e.ts
+++ src/components/combo-box/tests/suggest-behavior.e2e.ts
@@ -1,4 +1,5 @@
 import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
+import { simpleModelComboBox1 } from "../../../showcase/assets/components/combo-box/models";
 
 describe("[ch-combo-box-render][behavior][suggest]", () => {
   let page: E2EPage;
@@ -6,7 +7,8 @@ describe("[ch-combo-box-render][behavior][suggest]", () => {
 
   beforeEach(async () => {
     page = await newE2EPage({
-      html: `<ch-combo-box-render suggest></ch-combo-box-render>`,
+      html: `<button>Dummy button</button>
+      <ch-combo-box-render suggest></ch-combo-box-render>`,
       failOnConsoleError: true
     });
     comboBoxRef = await page.find("ch-combo-box-render");
@@ -26,5 +28,30 @@ describe("[ch-combo-box-render][behavior][suggest]", () => {
     );
 
     expect(pickerSize).toBe("0px");
+  });
+
+  it("should not render items if the popover is not displayed", async () => {
+    let popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeNull();
+
+    await comboBoxRef.setProperty("model", simpleModelComboBox1);
+    await page.waitForChanges();
+    popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeNull();
+  });
+
+  it("should destroy the items after the popover is closed", async () => {
+    await comboBoxRef.setProperty("model", simpleModelComboBox1);
+    await page.waitForChanges();
+    await page.click("ch-combo-box-render");
+    await page.waitForChanges();
+
+    let popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeDefined();
+
+    await page.click("button");
+    await page.waitForChanges();
+    popoverRef = await page.find("ch-combo-box-render >>> ch-popover");
+    expect(popoverRef).toBeNull();
   });
 });

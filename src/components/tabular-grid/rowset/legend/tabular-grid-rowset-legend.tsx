b82a5eb0232441ee19dd07fe3518feb59f75d1ce
Daniel Mariño <dmarino@genexus.com>
2024-10-04 16:16:50 -0300

    Add the property 'parts' to ch-tabular-grid-rowset-legend

--- src/components/tabular-grid/rowset/legend/tabular-grid-rowset-legend.tsx
+++ src/components/tabular-grid/rowset/legend/tabular-grid-rowset-legend.tsx
@@ -1,4 +1,16 @@
-import { Component, Listen, Event, EventEmitter, h, Host } from "@stencil/core";
+import {
+  Component,
+  Element,
+  Listen,
+  Event,
+  EventEmitter,
+  h,
+  Host,
+  Prop
+} from "@stencil/core";
+import { tokenMap } from "../../../../common/utils";
+import { TABULAR_GRID_PARTS_DICTIONARY } from "../../../../common/reserved-names";
+import HTMLChTabularGridRowsetElement from "../tabular-grid-rowset";
 
 /**
  * The `ch-tabular-grid-rowset-legend` component represents a caption for the `ch-tabular-grid-rowset` element.
@@ -9,6 +21,15 @@ import { Component, Listen, Event, EventEmitter, h, Host } from "@stencil/core";
   shadow: true
 })
 export class ChTabularGridRowsetLegend {
+  @Element() el: HTMLChTabularGridRowsetLegendElement;
+
+  /**
+   * A boolean or string that controls the parts applied to the legend.
+   * - When `true`, it automatically applies the part names "column" and the column's unique ID.
+   * - When a string is provided, it appends that string to the default part names "column" and the column's ID.
+   */
+  @Prop() readonly parts: boolean | string;
+
   /**
    * Event emitted when the legend is clicked.
    */
@@ -19,9 +40,23 @@ export class ChTabularGridRowsetLegend {
     this.rowsetLegendClicked.emit();
   }
 
+  #getRowsetId = () => {
+    const rowset = this.el.parentElement as HTMLChTabularGridRowsetElement;
+    return rowset?.rowsetId;
+  };
+
   render() {
     return (
-      <Host>
+      <Host
+        part={
+          this.parts &&
+          tokenMap({
+            [TABULAR_GRID_PARTS_DICTIONARY.ROWSET_LEGEND]: true,
+            [this.#getRowsetId()]: !!this.#getRowsetId(),
+            [this.parts.toString()]: typeof this.parts === "string"
+          })
+        }
+      >
         <div part="caret"></div>
         <label part="selector-label">
           <input type="checkbox" part="selector"></input>

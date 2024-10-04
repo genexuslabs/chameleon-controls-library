a033bd2a8de54261cae9026e6902ea49284d90fc
Daniel Mariño <dmarino@genexus.com>
2024-10-04 16:11:36 -0300

    Add the property 'parts' to ch-tabular-grid-columnset

--- src/components/tabular-grid/columnset/tabular-grid-columnset.tsx
+++ src/components/tabular-grid/columnset/tabular-grid-columnset.tsx
@@ -1,5 +1,7 @@
-import { Component, Element, Listen } from "@stencil/core";
+import { Component, Element, h, Host, Listen, Prop } from "@stencil/core";
 import { TabularGridColumnSortChangedEvent } from "../column/tabular-grid-column-types";
+import { TABULAR_GRID_PARTS_DICTIONARY } from "../../../common/reserved-names";
+import { tokenMap } from "../../../common/utils";
 
 /**
  * The `ch-tabular-grid-columnset` component represents a group of columns.
@@ -11,6 +13,13 @@ import { TabularGridColumnSortChangedEvent } from "../column/tabular-grid-column
 })
 export class ChTabularGridColumnset {
   @Element() el: HTMLChTabularGridColumnsetElement;
+
+  /**
+   * A boolean or string that controls the parts applied to the column.
+   * - When `true`, it automatically applies the part names "column" and the column's unique ID.
+   * - When a string is provided, it appends that string to the default part names "column" and the column's ID.
+   */
+  @Prop() readonly parts: boolean | string;
 
   @Listen("columnSortChanged")
   columnSortChangedHandler(
@@ -25,5 +34,19 @@ export class ChTabularGridColumnset {
         column.sortDirection = null;
       }
     });
+  }
+
+  render() {
+    return (
+      <Host
+        part={
+          this.parts &&
+          tokenMap({
+            [TABULAR_GRID_PARTS_DICTIONARY.COLUMNSET]: true,
+            [this.parts.toString()]: typeof this.parts === "string"
+          })
+        }
+      ></Host>
+    );
   }
 }

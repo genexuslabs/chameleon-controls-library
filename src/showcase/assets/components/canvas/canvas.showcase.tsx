ce685ac3ebb653c06303234ab1b409d751e335d5
Nicolás Cámera <ncamera@genexus.com>
2024-10-11 00:32:50 -0300

    Improve showcase story

--- src/showcase/assets/components/canvas/canvas.showcase.tsx
+++ src/showcase/assets/components/canvas/canvas.showcase.tsx
@@ -1,6 +1,7 @@
 import { h } from "@stencil/core";
 import {
   ShowcaseRenderProperties,
+  ShowcaseRenderProperty,
   ShowcaseStory,
   ShowcaseTemplatePropertyInfo
 } from "../types";
@@ -10,10 +11,20 @@ import {
   canvasSimple3Model,
   canvasSimpleModel
 } from "./models";
+import { CanvasPositionLimit } from "../../../../components";
+import { CanvasGridSettingsDots } from "../../../../components/canvas/types";
 
 const state: Partial<HTMLChCanvasElement> = {};
 
-const render = () => <ch-canvas class="canvas" model={state.model}></ch-canvas>;
+const render = () => (
+  <ch-canvas
+    class="canvas"
+    contextPositionLimit={state.contextPositionLimit}
+    drawGrid={state.drawGrid}
+    gridSettings={state.gridSettings}
+    model={state.model}
+  ></ch-canvas>
+);
 
 const showcaseRenderProperties: ShowcaseRenderProperties<HTMLChCanvasElement> =
   [
@@ -22,7 +33,7 @@ const showcaseRenderProperties: ShowcaseRenderProperties<HTMLChCanvasElement> =
       properties: [
         {
           id: "model",
-          caption: "Model",
+          accessibleName: "Model",
           type: "enum",
           value: canvasSimple3Model,
           values: [
@@ -30,6 +41,79 @@ const showcaseRenderProperties: ShowcaseRenderProperties<HTMLChCanvasElement> =
             { caption: "Simple model 2", value: canvasSimple2Model },
             { caption: "Simple model 3", value: canvasSimple3Model }
           ]
+        }
+      ]
+    },
+    {
+      caption: "Position",
+      properties: [
+        {
+          id: "contextPositionLimit",
+          caption: "Context Position Limit",
+          type: "object",
+          render: "independent-properties",
+          properties: [
+            {
+              id: "scaleLowerBound",
+              caption: "Scale Lower Bound",
+              value: 0.1,
+              type: "number"
+            },
+            {
+              id: "scaleUpperBound",
+              caption: "Scale Upper Bound",
+              value: 5,
+              type: "number"
+            }
+          ] satisfies ShowcaseRenderProperty<CanvasPositionLimit>[]
+        }
+      ]
+    },
+    {
+      caption: "Grid",
+      properties: [
+        {
+          id: "drawGrid",
+          caption: "Draw Grid",
+          type: "boolean",
+          value: true
+        },
+        {
+          id: "gridSettings",
+          caption: "Options",
+          type: "object",
+          render: "independent-properties",
+          properties: [
+            {
+              id: "type",
+              caption: "Type",
+              value: "mesh",
+              values: [
+                { caption: "Mesh", value: "mesh" },
+                { caption: "Dots", value: "dots" }
+              ],
+              type: "enum"
+            },
+            {
+              id: "color",
+              caption: "Color",
+              value: "#000", // rgb(199,199,199)
+              type: "string"
+            },
+            // TODO: Does not work.
+            // {
+            //   id: "size",
+            //   caption: "Size",
+            //   value: 40,
+            //   type: "number"
+            // },
+            {
+              id: "dotSize",
+              caption: "Dot Size",
+              value: 2,
+              type: "number"
+            }
+          ] satisfies ShowcaseRenderProperty<CanvasGridSettingsDots>[]
         }
       ]
     }

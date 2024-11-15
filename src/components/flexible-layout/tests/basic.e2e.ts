3195df240a7612ab17c541ad8e5a8452f13ff34f
Nicolás Cámera <ncamera@genexus.com>
2024-11-14 23:46:27 -0300

    Add unit tests

---
+++ src/components/flexible-layout/tests/basic.e2e.ts
@@ -0,0 +1,60 @@
+import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
+import { FlexibleLayoutModel } from "../internal/flexible-layout/types";
+
+const EMPTY_MODEL = {
+  id: "root",
+  direction: "columns",
+  items: []
+} satisfies FlexibleLayoutModel;
+
+// const FLEXIBLE_LAYOUT_RENDERED_CONTENT = (children: string) =>
+//   `<ch-flexible-layout class="flexible-layout hydrated">${children}</ch-flexible-layout>`;
+
+describe("[ch-flexible-layout-render][basic]", () => {
+  let page: E2EPage;
+  let flexibleLayoutRef: E2EElement;
+
+  // const getRenderedContent = () =>
+  //   page.evaluate(
+  //     () =>
+  //       document.querySelector("ch-flexible-layout-render").shadowRoot.innerHTML
+  //   );
+
+  beforeEach(async () => {
+    page = await newE2EPage({
+      html: `<ch-flexible-layout-render></ch-flexible-layout-render>`,
+      failOnConsoleError: true
+    });
+    flexibleLayoutRef = await page.find("ch-flexible-layout-render");
+  });
+
+  it("should have a shadowRoot", async () => {
+    expect(flexibleLayoutRef.shadowRoot).toBeTruthy();
+  });
+
+  it('should not throw any error when the "model" property is undefined', async () => {
+    flexibleLayoutRef.setProperty("model", undefined);
+    await page.waitForChanges();
+    expect(await flexibleLayoutRef.getProperty("model")).toBe(undefined);
+  });
+
+  it('should not throw any error when the "renders" property is undefined', async () => {
+    flexibleLayoutRef.setProperty("renders", undefined);
+    await page.waitForChanges();
+    expect(await flexibleLayoutRef.getProperty("renders")).toBe(undefined);
+  });
+
+  it('the "model" and "renders" property should be undefined by default', async () => {
+    expect(await flexibleLayoutRef.getProperty("model")).toBe(undefined);
+    expect(await flexibleLayoutRef.getProperty("renders")).toBe(undefined);
+  });
+
+  it('should not throw any error when the "renders" property is undefined and the "model" property is set without any widget', async () => {
+    flexibleLayoutRef.setProperty("model", EMPTY_MODEL);
+    flexibleLayoutRef.setProperty("renders", undefined);
+    await page.waitForChanges();
+
+    expect(await flexibleLayoutRef.getProperty("model")).toEqual(EMPTY_MODEL);
+    expect(await flexibleLayoutRef.getProperty("renders")).toBe(undefined);
+  });
+});
---
+++ src/components/flexible-layout/tests/slots.e2e.ts
@@ -0,0 +1,51 @@
+import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
+import { FlexibleLayoutModel } from "../internal/flexible-layout/types";
+
+const TEST1_ID = "Test";
+
+const ITEM_WITH_SLOT_MODEL = {
+  id: "root",
+  direction: "columns",
+  items: [
+    {
+      id: TEST1_ID,
+      size: "1fr",
+      type: "single-content",
+      widget: { id: TEST1_ID, name: "", slot: true }
+    }
+  ]
+} satisfies FlexibleLayoutModel;
+
+const FLEXIBLE_LAYOUT_RENDERED_CONTENT = (children: string) =>
+  `<ch-flexible-layout class="flexible-layout hydrated">${children}</ch-flexible-layout>`;
+
+describe("[ch-flexible-layout-render][slots]", () => {
+  let page: E2EPage;
+  let flexibleLayoutRef: E2EElement;
+
+  const getRenderedContent = () =>
+    page.evaluate(
+      () =>
+        document.querySelector("ch-flexible-layout-render").shadowRoot.innerHTML
+    );
+
+  beforeEach(async () => {
+    page = await newE2EPage({
+      html: `<ch-flexible-layout-render></ch-flexible-layout-render>`,
+      failOnConsoleError: true
+    });
+    flexibleLayoutRef = await page.find("ch-flexible-layout-render");
+  });
+
+  it('should render an slot when the model only contains an item with "slot: true"', async () => {
+    flexibleLayoutRef.setProperty("model", ITEM_WITH_SLOT_MODEL);
+    await page.waitForChanges();
+    await page.waitForChanges();
+
+    expect(await getRenderedContent()).toBe(
+      FLEXIBLE_LAYOUT_RENDERED_CONTENT(
+        `<slot name="${TEST1_ID}" slot="${TEST1_ID}"></slot>`
+      )
+    );
+  });
+});

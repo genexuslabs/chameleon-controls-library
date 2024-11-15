63f45ebf40830b6a6b4d6ceb1cdf5b79ed2bb1d2
Nicolás Cámera <ncamera@genexus.com>
2024-11-15 11:21:43 -0300

    Add more unit tests

--- src/components/flexible-layout/tests/slots.e2e.ts
+++ src/components/flexible-layout/tests/slots.e2e.ts
@@ -5,10 +5,14 @@ import {
   newE2EPage
 } from "@stencil/core/testing";
 import { FlexibleLayoutModel } from "../internal/flexible-layout/types";
+import {
+  flexibleLayoutTestRenders,
+  TEST1_ID,
+  TEST2_ID,
+  TEST3_ID
+} from "./renders-test";
 
-const TEST1_ID = "Test";
-
-const ITEM_WITH_SLOT_MODEL = {
+const ONE_SLOT_MODEL = {
   id: "root",
   direction: "columns",
   items: [
@@ -21,8 +25,48 @@ const ITEM_WITH_SLOT_MODEL = {
   ]
 } satisfies FlexibleLayoutModel;
 
+const TWO_SLOT_MODEL = {
+  id: "root",
+  direction: "columns",
+  items: [
+    {
+      id: TEST1_ID,
+      size: "1fr",
+      type: "single-content",
+      widget: { id: TEST1_ID, name: "", slot: true }
+    },
+    {
+      id: TEST2_ID,
+      size: "1fr",
+      type: "single-content",
+      widget: { id: TEST2_ID, name: "", slot: true }
+    }
+  ]
+} satisfies FlexibleLayoutModel;
+
+const SLOT_AND_RENDER_MODEL = {
+  id: "root",
+  direction: "columns",
+  items: [
+    {
+      id: TEST1_ID,
+      size: "1fr",
+      type: "single-content",
+      widget: { id: TEST1_ID, name: "", slot: true }
+    },
+    {
+      id: TEST3_ID,
+      size: "1fr",
+      type: "single-content",
+      widget: { id: TEST3_ID, name: "", renderId: TEST3_ID }
+    }
+  ]
+} satisfies FlexibleLayoutModel;
+
 const FLEXIBLE_LAYOUT_RENDERED_CONTENT = (children: string) =>
   `<ch-flexible-layout class="flexible-layout hydrated">${children}</ch-flexible-layout>`;
+
+const SLOT_CONTENT = (id: string) => `<slot name="${id}" slot="${id}"></slot>`;
 
 describe("[ch-flexible-layout-render][slots]", () => {
   let page: E2EPage;
@@ -47,18 +91,75 @@ describe("[ch-flexible-layout-render][slots]", () => {
   });
 
   it('should render an slot when the model only contains an item with "slot: true"', async () => {
-    flexibleLayoutRef.setProperty("model", ITEM_WITH_SLOT_MODEL);
+    flexibleLayoutRef.setProperty("model", ONE_SLOT_MODEL);
     await page.waitForChanges();
 
     expect(await getRenderedContent()).toBe(
-      FLEXIBLE_LAYOUT_RENDERED_CONTENT(
-        `<slot name="${TEST1_ID}" slot="${TEST1_ID}"></slot>`
-      )
+      FLEXIBLE_LAYOUT_RENDERED_CONTENT(SLOT_CONTENT(TEST1_ID))
     );
     expect(renderedWidgetsChangeSpy).toHaveReceivedEvent();
     expect(renderedWidgetsChangeSpy).toHaveReceivedEventDetail({
       rendered: [],
       slotted: [TEST1_ID]
+    });
+  });
+
+  it('should render two slots when the model contains items item with "slot: true"', async () => {
+    flexibleLayoutRef.setProperty("model", TWO_SLOT_MODEL);
+    await page.waitForChanges();
+
+    expect(await getRenderedContent()).toBe(
+      FLEXIBLE_LAYOUT_RENDERED_CONTENT(
+        SLOT_CONTENT(TEST1_ID) + SLOT_CONTENT(TEST2_ID)
+      )
+    );
+    expect(renderedWidgetsChangeSpy).toHaveReceivedEvent();
+    expect(renderedWidgetsChangeSpy).toHaveReceivedEventDetail({
+      rendered: [],
+      slotted: [TEST1_ID, TEST2_ID]
+    });
+  });
+
+  // TODO: It seems that puppeteer do not allow us to serialize the h function
+  // from StencilJS to support the "renders" property
+  it.skip("should render slots and traditional items with renders", async () => {
+    flexibleLayoutRef.setProperty("model", SLOT_AND_RENDER_MODEL);
+    flexibleLayoutRef.setProperty("renders", flexibleLayoutTestRenders);
+    await page.waitForChanges();
+
+    expect(await getRenderedContent()).toBe(
+      FLEXIBLE_LAYOUT_RENDERED_CONTENT(
+        SLOT_CONTENT(TEST1_ID) +
+          `<button slot="${TEST3_ID}" type="button">Something</button>`
+      )
+    );
+    expect(renderedWidgetsChangeSpy).toHaveReceivedEvent();
+    expect(renderedWidgetsChangeSpy).toHaveReceivedEventDetail({
+      rendered: [TEST3_ID],
+      slotted: [TEST1_ID]
+    });
+  });
+
+  it("should update the rendered slots when updating the model at runtime", async () => {
+    flexibleLayoutRef.setProperty("model", ONE_SLOT_MODEL);
+    await page.waitForChanges();
+
+    flexibleLayoutRef.setProperty("model", TWO_SLOT_MODEL);
+    await page.waitForChanges();
+
+    expect(await getRenderedContent()).toBe(
+      FLEXIBLE_LAYOUT_RENDERED_CONTENT(
+        SLOT_CONTENT(TEST1_ID) + SLOT_CONTENT(TEST2_ID)
+      )
+    );
+    expect(renderedWidgetsChangeSpy).toHaveReceivedEventTimes(2);
+    expect(renderedWidgetsChangeSpy).toHaveNthReceivedEventDetail(0, {
+      rendered: [],
+      slotted: [TEST1_ID]
+    });
+    expect(renderedWidgetsChangeSpy).toHaveNthReceivedEventDetail(1, {
+      rendered: [],
+      slotted: [TEST1_ID, TEST2_ID]
     });
   });
 });

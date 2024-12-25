3daf5c9d1d79b8d51fb47554f52fd7a93b315314
Nicolas Camera Lopez <ncamera@genexus.com>
2024-12-24 21:26:21 -0300

    Add basic unit tests

---
+++ src/components/textblock/tests/basic.e2e.ts
@@ -0,0 +1,42 @@
+import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
+
+describe("[ch-textblock][basic]", () => {
+  let page: E2EPage;
+  let tooltipRef: E2EElement;
+
+  beforeEach(async () => {
+    page = await newE2EPage({
+      html: `<ch-textblock></ch-textblock>`,
+      failOnConsoleError: true
+    });
+    tooltipRef = await page.find("ch-textblock");
+  });
+
+  const testDefault = (
+    propertyName: string,
+    propertyValue: any,
+    propertyValueDescription: string
+  ) =>
+    it(`the "${propertyName}" property should be ${
+      typeof propertyValue === "string"
+        ? `"${propertyValueDescription}"`
+        : propertyValueDescription
+    } by default`, async () => {
+      expect(await tooltipRef.getProperty(propertyName)).toBe(propertyValue);
+    });
+
+  it("should have Shadow DOM", async () => {
+    expect(tooltipRef.shadowRoot).toBeTruthy();
+  });
+
+  testDefault("autoGrow", false, "false");
+  testDefault("caption", undefined, "undefined");
+  testDefault("characterToMeasureLineHeight", "A", "A");
+  testDefault("format", "text", "text");
+  testDefault("highlightPattern", undefined, "undefined");
+  testDefault("showTooltipOnOverflow", false, "false");
+
+  it('should have "display: inline-grid" by default', async () => {
+    expect((await tooltipRef.getComputedStyle()).display).toBe("inline-grid");
+  });
+});

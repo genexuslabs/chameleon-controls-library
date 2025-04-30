d95b876b47a04ae81e952a73bbe4ebba47c53484
Nicolás Cámera <ncamera@genexus.com>
2025-04-29 22:23:35 -0300

    Fix copy-files.mjs not working properly

--- copy-files.mjs
+++ copy-files.mjs
@@ -1,17 +1,23 @@
-import fs from "fs";
+import { existsSync } from "node:fs";
+import { copyFile, mkdir } from "node:fs/promises";
 
-fs.copyFile(
+await copyFile(
   "./src/framework-integrations.ts",
-  "./dist/collection/framework-integrations.ts",
+  "./dist/collection/framework-integrations.ts"
+).catch(err => {
+  if (err) {
+    throw err;
+  }
+});
+
+if (!existsSync("./dist/react/")) {
+  await mkdir("./dist/react/");
+}
+
+await copyFile("./src/copy-react.js", "./dist/react/copy-react.js").catch(
   err => {
     if (err) {
       throw err;
     }
   }
 );
-
-fs.copyFile("./src/copy-react.js", "./dist/react/copy-react.js", err => {
-  if (err) {
-    throw err;
-  }
-});

53fe42d1426c38acc2ddea824115361c70a80b3b
Nicolás Cámera <81186472+ncamera@users.noreply.github.com>
2025-06-03 14:31:30 -0300

    `[ch-chat]` Fix sequential calls for `addNewMessage` not working when the chat is empty and add `sendChatMessage` method (#513)
    * Fix double addNewMessage not working when the chat is empty
    
    * Fix package.json
    
    * Move test file
    
    * Create send-message-by-user-interaction.e2e.ts
    
    * Add support for sendChatMessage method
    
    Send the current message of the ch-chat's `send-input` element.
      - If the `content` parameter is provided, it will be used in replacement of the input content.
     - If the `files` parameter is provided, the `getChatMessageFiles` callback won't be executed to get the current files of the chat.
    
    Whether or not the `content` parameter is provided, the content of the input will be cleared.
    
    * Update package.json
    
    * Update focusChatInput.e2e.ts
    
    * Create addNewMessage.e2e.ts
    
    * Update docs
    
    * Update docs

--- src/components/chat/tests/methods/focusChatInput.e2e.ts
+++ src/components/chat/tests/methods/focusChatInput.e2e.ts
@@ -1,10 +1,10 @@
 import { E2EElement, E2EPage, newE2EPage } from "@stencil/core/testing";
-import { isActiveElement } from "../../../testing/utils.e2e";
-import type { SmartGridDataState } from "../../smart-grid/internal/infinite-scroll/types";
+import { isActiveElement } from "../../../../testing/utils.e2e";
+import type { SmartGridDataState } from "../../../smart-grid/internal/infinite-scroll/types";
 
 const TEXTAREA_SELECTOR = "ch-chat >>> ch-edit >>> textarea";
 
-describe("[ch-chat][methods]", () => {
+describe("[ch-chat][focusChatInput]", () => {
   let page: E2EPage;
   let chatRef: E2EElement;
 
@@ -16,7 +16,7 @@ describe("[ch-chat][methods]", () => {
     chatRef = await page.find("ch-chat");
   });
 
-  it("[focusChatInput] should focus the textarea", async () => {
+  it("should focus the textarea", async () => {
     expect(await isActiveElement(page, TEXTAREA_SELECTOR)).toBeFalsy();
 
     await chatRef.callMethod("focusChatInput");
@@ -26,7 +26,7 @@ describe("[ch-chat][methods]", () => {
   });
 
   const runTestFocusWithLoadingState = (loadingState: SmartGridDataState) =>
-    it(`[focusChatInput] should focus the textarea, loadingState = "${loadingState}"`, async () => {
+    it(`should focus the textarea, loadingState = "${loadingState}"`, async () => {
       chatRef.setProperty("loadingState", loadingState);
       await page.waitForChanges();
 

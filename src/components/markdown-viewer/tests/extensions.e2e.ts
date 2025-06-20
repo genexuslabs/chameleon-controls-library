86fceac03967bdbf5ffc4089c4a281fca41f3277
Nicolás Cámera <ncamera@genexus.com>
2025-06-20 10:58:52 -0300

    Create extensions.e2e.ts

---
+++ src/components/markdown-viewer/tests/extensions.e2e.ts
@@ -0,0 +1,8 @@
+describe("[ch-markdown-viewer][extensions]", () => {
+  // let page: E2EPage;
+  // let markdownViewerRef: E2EElement;
+
+  it.todo(
+    "should render all possible markdown, even if there are token nodes that are not mapped"
+  );
+});

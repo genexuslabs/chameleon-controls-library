genexuslabs/chameleon-controls-library

commit 8544295addf83eed45a172bbef8bb2288d3698a1

`[ch-popover]` Fixed positionTry not working when using inlineSizeMatch/blockSizeMatch with "action-element-as-minimum" (#554)

src/components/popover/popover.tsx
@@ -806,7 +806,9 @@ export class ChPopover {
   ) => {
     if (this.inlineSizeMatch === "action-element-as-minimum") {
       setProperty(this.el, POPOVER_MIN_INLINE_SIZE, actionRect.width);
-      return actionRect.width;
+
+      // TODO: Add e2e tests for this
+      return Math.max(actionRect.width, popoverRect.width);
     }
 
     // Size is determined by the content
@@ -825,12 +827,9 @@ export class ChPopover {
   ) => {
     if (this.blockSizeMatch === "action-element-as-minimum") {
       setProperty(this.el, POPOVER_MIN_BLOCK_SIZE, actionRect.height);
-      return actionRect.height;
-    }
 
-    // Size is determined by the content
-    if (this.#resizeWasMade || this.blockSizeMatch === "content") {
-      return popoverRect.height;
+      // TODO: Add e2e tests for this
+      return Math.max(actionRect.height, popoverRect.height);
     }
 
     // Size is the same as the `actionElement`

commit 0befcac0413afc042935fe40babf071a47f526d8

`[ch-combo-box-render]` Added support to customize the max size of the popover and fixed `ch-popover` not enforcing maximum sizes (#553)
* Fixed --ch-popover-max-block-size and --ch-popover-max-inline-size custom vars not working in the ch-popover

* `[ch-combo-box-render]` Add support to customize the max size of the popover

The --ch-combo-box__popover-max-block-size and --ch-combo-box__popover-max-inline-size custom vars are added to the ch-combo-box-render to support customizing the maximum size of the popover.

* Update readmes

* Fix e2e tests

* Fix minor regression introduced by #554

src/components/combo-box/combo-box.scss
@@ -90,6 +90,20 @@
   --ch-combo-box__placeholder-color: currentColor;
 
   /**
+   * @prop --ch-combo-box__popover-max-block-size:
+   * Specifies the maximum block size of the popover. Only px values are supported.
+   * @default auto
+   */
+  --ch-combo-box__popover-max-block-size: auto;
+
+  /**
+   * @prop --ch-combo-box__popover-max-inline-size:
+   * Specifies the maximum inline size of the popover. Only px values are supported.
+   * @default auto
+   */
+  --ch-combo-box__popover-max-inline-size: auto;
+
+  /**
    * @prop --ch-combo-box-item-gap:
    * Specifies the spacing between the images, text and the expandable button
    * on items.
@@ -209,6 +223,9 @@ select {
 //           Separation
 // - - - - - - - - - - - - - - - -
 ch-popover {
+  // TODO: Add e2e tests for these assignments
+  --ch-popover-max-inline-size: var(--ch-combo-box__popover-max-inline-size);
+  --ch-popover-max-block-size: var(--ch-combo-box__popover-max-block-size);
   --ch-popover-separation-x: var(--ch-combo-box-separation-x);
   --ch-popover-separation-y: var(--ch-combo-box-separation-y);
 

src/components/popover/popover.scss
@@ -31,7 +31,7 @@ $ch-popover-y--same-layer: calc(
   /**
    * @prop --ch-popover-max-block-size:
    * Specifies the maximum block size of the popover. Useful for scenarios where the
-   * popover is resizable.
+   * popover is resizable. Only px values are supported.
    * @default auto
    */
   --ch-popover-max-block-size: auto;
@@ -39,7 +39,7 @@ $ch-popover-y--same-layer: calc(
   /**
      * @prop --ch-popover-max-inline-size:
      * Specifies the maximum inline size of the popover. Useful for scenarios
-     * where the popover is resizable.
+     * where the popover is resizable. Only px values are supported.
      * @default auto
      */
   --ch-popover-max-inline-size: auto;

src/components/popover/popover.tsx
@@ -49,6 +49,9 @@ const POPOVER_INLINE_SIZE = "--ch-popover-inline-size";
 const POPOVER_MIN_BLOCK_SIZE = "--ch-popover-min-block-size";
 const POPOVER_MIN_INLINE_SIZE = "--ch-popover-min-inline-size";
 
+const POPOVER_MAX_BLOCK_SIZE = "--ch-popover-max-block-size";
+const POPOVER_MAX_INLINE_SIZE = "--ch-popover-max-inline-size";
+
 const POPOVER_FORCED_MAX_BLOCK_SIZE = "--ch-popover-forced-max-block-size";
 const POPOVER_FORCED_MAX_INLINE_SIZE = "--ch-popover-forced-max-inline-size";
 
@@ -721,12 +724,44 @@ export class ChPopover {
       popoverScrollSizes
     );
 
+    // TODO: Add e2e tests for this
+    // We need to check if a maximum width/height for the popover was defined
+    // to give more priority to that value, instead of the actual width/height,
+    // even if the width/height comes from "action-element-as-minimum"
+    const maxInlineSizeCustomVarValue = computedStyle.getPropertyValue(
+      POPOVER_MAX_INLINE_SIZE
+    );
+    const maxBlockSizeCustomVarValue = computedStyle.getPropertyValue(
+      POPOVER_MAX_BLOCK_SIZE
+    );
+
+    let actualPopoverWidth = popoverWidth;
+    let actualPopoverHeight = popoverHeight;
+
+    // We only support "px" for the max-block-size and max-inline-size custom
+    // var values
+    // TODO: Add e2e tests for this
+    try {
+      if (maxInlineSizeCustomVarValue.endsWith("px")) {
+        actualPopoverWidth = Number(
+          maxInlineSizeCustomVarValue.replace("px", "").trim()
+        );
+      }
+      if (maxBlockSizeCustomVarValue.endsWith("px")) {
+        actualPopoverHeight = Number(
+          maxBlockSizeCustomVarValue.replace("px", "").trim()
+        );
+      }
+    } catch {
+      //
+    }
+
     const alignment = setResponsiveAlignment(
       documentRect,
       actionRect,
       actionInlineStart,
-      popoverWidth,
-      popoverHeight,
+      actualPopoverWidth,
+      actualPopoverHeight,
       computedStyle,
       this.inlineAlign,
       this.blockAlign,
@@ -737,8 +772,8 @@ export class ChPopover {
     const blockOverflow = alignment[1].alignmentOverflow;
 
     this.#setOverflowBehavior(
-      popoverWidth,
-      popoverHeight,
+      actualPopoverWidth,
+      actualPopoverHeight,
       inlineOverflow,
       blockOverflow
     );
@@ -830,6 +865,11 @@ export class ChPopover {
 
       // TODO: Add e2e tests for this
       return Math.max(actionRect.height, popoverRect.height);
+    }
+
+    // Size is determined by the content
+    if (this.#resizeWasMade || this.blockSizeMatch === "content") {
+      return popoverRect.height;
     }
 
     // Size is the same as the `actionElement`
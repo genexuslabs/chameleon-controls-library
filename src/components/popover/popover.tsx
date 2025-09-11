genexuslabs/chameleon-controls-library

commit 8544295addf83eed45a172bbef8bb2288d3698a1

`[ch-popover]` Fixed positionTry not working when using inlineSizeMatch/blockSizeMatch with "action-element-as-minimum" (#554)

src/components/popover/popover.tsx
@@ -806,7 +806,9 @@ export class ChPopover {
   ) => {
     if (this.inlineSizeMatch === "action-element-as-minimum") {
       setProperty(this.el, POPOVER_MIN_INLINE_SIZE, actionRect.width);
-      return actionRect.width;
+
+      // TODO: Add e2e tests for this
+      return Math.max(actionRect.width, popoverRect.width);
     }
 
     // Size is determined by the content
@@ -825,12 +827,9 @@ export class ChPopover {
   ) => {
     if (this.blockSizeMatch === "action-element-as-minimum") {
       setProperty(this.el, POPOVER_MIN_BLOCK_SIZE, actionRect.height);
-      return actionRect.height;
-    }
 
-    // Size is determined by the content
-    if (this.#resizeWasMade || this.blockSizeMatch === "content") {
-      return popoverRect.height;
+      // TODO: Add e2e tests for this
+      return Math.max(actionRect.height, popoverRect.height);
     }
 
     // Size is the same as the `actionElement`

commit 32bf076a2947c2ceeb12292a4bfb02ebb3a778dc

Fix minor regression introduced by #554

src/components/popover/popover.tsx
@@ -867,6 +867,11 @@ export class ChPopover {
       return Math.max(actionRect.height, popoverRect.height);
     }
 
+    // Size is determined by the content
+    if (this.#resizeWasMade || this.blockSizeMatch === "content") {
+      return popoverRect.height;
+    }
+
     // Size is the same as the `actionElement`
     setProperty(this.el, POPOVER_BLOCK_SIZE, actionRect.height);
     return actionRect.height;
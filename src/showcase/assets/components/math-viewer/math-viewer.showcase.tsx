86f2afa64d6044f1f00f0982d4fce9cc1b8c5199
ncamera <ncamera@genexus.com>
2025-11-05 12:04:47 -0300

    Improve the math-viewer showcase page

--- src/showcase/assets/components/math-viewer/math-viewer.showcase.tsx
+++ src/showcase/assets/components/math-viewer/math-viewer.showcase.tsx
@@ -4,13 +4,7 @@ import { updateShowcase } from "../utils";
 import { markdownReadmeModel } from "./models";
 
 let initialMarkdown = markdownReadmeModel;
-let rawHTMLEnabled = "false";
-let showIndicator = "false";
 
-console.log(rawHTMLEnabled, showIndicator);
-
-let checkboxRawHTMLRef: HTMLChCheckboxElement;
-let checkboxIndicatorRef: HTMLChCheckboxElement;
 let textareaRef: HTMLTextAreaElement;
 
 const handleValueChange = () => {
@@ -22,35 +16,9 @@ const handleValueChange = () => {
   updateShowcase();
 };
 
-const handleCheckboxValueChange = () => {
-  rawHTMLEnabled = checkboxRawHTMLRef.value;
-  showIndicator = checkboxIndicatorRef.value;
-
-  // TODO: Until we support external slots in the ch-flexible-layout-render,
-  // this is a hack to update the render of the widget and thus re-render the
-  // combo-box updating the displayed items
-  updateShowcase();
-};
-
 const render: ShowcaseRender = () => (
   <div class="markdown-test-main-wrapper">
-    <div class="markdown-test-properties">
-      <ch-checkbox
-        caption="Raw HTML enabled"
-        class="checkbox"
-        checkedValue="true"
-        onInput={handleCheckboxValueChange}
-        ref={el => (checkboxRawHTMLRef = el)}
-      ></ch-checkbox>
-
-      <ch-checkbox
-        caption="Show indicator"
-        class="checkbox"
-        checkedValue="true"
-        onInput={handleCheckboxValueChange}
-        ref={el => (checkboxIndicatorRef = el)}
-      ></ch-checkbox>
-    </div>
+    <div class="markdown-test-properties"></div>
 
     <textarea
       aria-label="Markdown content"

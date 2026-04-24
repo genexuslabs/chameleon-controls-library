61506e2bde0aebc2bf01fc91558c70aa2b7a9906
Nicolas Camera <ncamera@genexus.com>
2026-04-24 12:19:04 -0300

    Fix some vulnerabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from "vite";
-import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
-  plugins: [
-    // Minify JS
-    terser({
-      ecma: 2022,
-      module: true,
-      warnings: true,
-      compress: true,
-      format: {
-        comments: false // TODO: Check if we can do this due to license
-      }
-    })
-  ],
-
   // Bundles
   build: {
     assetsDir: "./assets",
